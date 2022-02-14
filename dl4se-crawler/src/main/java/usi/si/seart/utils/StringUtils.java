fdaf1787aa13e74d01a6dc0b70f453a40658a6d8
dabico <dabico@usi.ch>
2022-02-14 15:58:12 +0100

    Small changes to sha256

--- dl4se-crawler/src/main/java/usi/si/seart/utils/StringUtils.java
+++ dl4se-crawler/src/main/java/usi/si/seart/utils/StringUtils.java
@@ -6,6 +6,7 @@ import lombok.experimental.UtilityClass;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Objects;
 
 @UtilityClass
 public class StringUtils {
@@ -15,20 +16,20 @@ public class StringUtils {
      *
      * @param input An input {@link String} of arbitrary length.
      * @return The SHA-256 hashing algorithm result.
-     * @apiNote We suppress {@link NoSuchAlgorithmException}, as it will never occur with the fixed input.
+     * @apiNote We suppress throws of {@link NoSuchAlgorithmException}.
      * @see <a href="https://stackoverflow.com/a/11009612/17173324">Stack Overflow Thread</a>
      * @author user1452273
      * @author dabico
      */
     @SneakyThrows({NoSuchAlgorithmException.class})
     public static String sha256(String input) {
+        Objects.requireNonNull(input);
         MessageDigest md = MessageDigest.getInstance("SHA-256");
         byte[] hash = md.digest(input.getBytes(StandardCharsets.UTF_8));
         StringBuilder hexString = new StringBuilder();
         for (byte b : hash) {
-            final String hex = Integer.toHexString(0xff & b);
-            if (hex.length() == 1)
-                hexString.append('0');
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) hexString.append('0');
             hexString.append(hex);
         }
         return hexString.toString();
