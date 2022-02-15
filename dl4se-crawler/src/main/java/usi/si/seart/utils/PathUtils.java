b6d2fb9484c01bc0e6e1bf118b6ac551711a6a1b
dabico <dabico@usi.ch>
2022-02-15 17:43:37 +0100

    Added utility method to check if the Path points to a test file

--- dl4se-crawler/src/main/java/usi/si/seart/utils/PathUtils.java
+++ dl4se-crawler/src/main/java/usi/si/seart/utils/PathUtils.java
@@ -3,11 +3,69 @@ package usi.si.seart.utils;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 
+import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.nio.file.PathMatcher;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @UtilityClass
 public class PathUtils {
+
+    private final PathMatcher testPathMatcher;
+    static {
+        String globPattern = Stream.of(
+                "**/test/**",
+                "**/tests/**",
+                "**/Test*.java",
+                "**/*Test.java",
+                "**/*Tests.java",
+                "**/*TestCase.java",
+                "**/IT*.java",
+                "**/*IT.java",
+                "**/*ITCase.java"
+        ).collect(Collectors.joining(",", "glob:{", "}"));
+        testPathMatcher = FileSystems.getDefault().getPathMatcher(globPattern);
+    }
+
+
+    /**
+     * Path utility used to determine if a file is indeed a Java test file.
+     * We define test files as files whose:
+     *
+     * <ul>
+     *     <li>
+     *         Path contains the following directories:
+     *         <ul>
+     *             <li>{@code test}</li>
+     *             <li>{@code tests}</li>
+     *         </ul>
+     *     </li>
+     *     <li>
+     *         Name matches the following patterns:
+     *         <ul>
+     *             <li>{@code Test*.java}</li>
+     *             <li>{@code *Test.java}</li>
+     *             <li>{@code *Tests.java}</li>
+     *             <li>{@code *TestCase.java}</li>
+     *             <li>{@code IT*.java}</li>
+     *             <li>{@code *IT.java}</li>
+     *             <li>{@code *ITCase.java}</li>
+     *         </ul>
+     *     </li>
+     * </ul>
+     *
+     * @implNote We keep the matching restrictive to minimise the amount of false positives.
+     * For instance, using a more general pattern {@code **test**} would match non-conforming cases like
+     * {@code /src/latest/App.java}.
+     * @param file Path to file that we are testing against.
+     * @return Whether the file in question is a test file.
+     */
+    // TODO: 15.02.22 Generalize to other languages
+    public boolean isTestFile(Path file) {
+        return testPathMatcher.matches(file);
+    }
 
     /**
      * Path utility used to forcefully delete a directory and all its contents.
--- dl4se-crawler/src/test/java/usi/si/seart/utils/PathUtilsTest.java
+++ dl4se-crawler/src/test/java/usi/si/seart/utils/PathUtilsTest.java
@@ -23,4 +23,26 @@ class PathUtilsTest {
         PathUtils.forceDelete(temp);
         Assertions.assertFalse(temp.toFile().exists());
     }
+
+    @Test
+    void isTestFile() {
+        Path file1 = Path.of("/java/src/org/json/App.java");
+        Path file2 = Path.of("/java/src/org/json/TestApp.java");
+        Path file3 = Path.of("/java/src/org/json/AppTest.java");
+        Path file4 = Path.of("/java/src/org/json/AppTests.java");
+        Path file5 = Path.of("/java/src/org/json/AppTestCase.java");
+        Path file6 = Path.of("/java/test/org/json/App.java");
+        Path file7 = Path.of("/java/tests/org/json/App.java");
+        Path file8 = Path.of("/test/org/json/App.java");
+        Path file9 = Path.of("/AppTest.java");
+        Assertions.assertFalse(PathUtils.isTestFile(file1));
+        Assertions.assertTrue(PathUtils.isTestFile(file2));
+        Assertions.assertTrue(PathUtils.isTestFile(file3));
+        Assertions.assertTrue(PathUtils.isTestFile(file4));
+        Assertions.assertTrue(PathUtils.isTestFile(file5));
+        Assertions.assertTrue(PathUtils.isTestFile(file6));
+        Assertions.assertTrue(PathUtils.isTestFile(file7));
+        Assertions.assertTrue(PathUtils.isTestFile(file8));
+        Assertions.assertTrue(PathUtils.isTestFile(file9));
+    }
 }
