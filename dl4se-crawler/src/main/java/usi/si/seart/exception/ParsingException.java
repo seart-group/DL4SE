ad0e99f6da017f28c81a6b0a91e066fb886b4dd2
dabico <dabico@usi.ch>
2022-02-23 18:27:23 +0100

    Added `ParsingException`
    This project-specific runtime exception will be used as a general exception
    that all parsers throw in the event of failure.

---
+++ dl4se-crawler/src/main/java/usi/si/seart/exception/ParsingException.java
@@ -0,0 +1,8 @@
+package usi.si.seart.exception;
+
+public class ParsingException extends RuntimeException {
+
+    public ParsingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
