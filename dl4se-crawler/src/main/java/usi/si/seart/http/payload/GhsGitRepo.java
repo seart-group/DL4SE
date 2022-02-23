87e9b2bcf86b5cf4366861fd8452b33b5354c9b6
dabico <dabico@usi.ch>
2022-02-23 09:38:37 +0100

    Added method for converting a GHS Payload into a `GitRepoBuilder`

--- dl4se-crawler/src/main/java/usi/si/seart/http/payload/GhsGitRepo.java
+++ dl4se-crawler/src/main/java/usi/si/seart/http/payload/GhsGitRepo.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
+import usi.si.seart.model.GitRepo;
 
+import java.time.ZoneId;
 import java.util.Date;
 import java.util.Map;
 import java.util.Set;
@@ -45,4 +47,18 @@ public class GhsGitRepo {
     Boolean isArchived;
     Map<String, Long> languages;
     Set<String> labels;
+
+    public GitRepo.GitRepoBuilder toGitRepoBuilder() {
+        GitRepo.GitRepoBuilder builder = GitRepo.builder();
+        builder.name(name);
+        builder.license(license);
+        builder.isFork(isFork);
+        if (commits != null) builder.commits(commits);
+        if (contributors != null) builder.contributors(contributors);
+        if (totalIssues != null) builder.issues(totalIssues);
+        if (stargazers != null) builder.stars(stargazers);
+        builder.lastUpdate(pushedAt.toInstant().atZone(ZoneId.of("UTC")).toLocalDateTime());
+        builder.lastCommitSHA(lastCommitSHA);
+        return builder;
+    }
 }
