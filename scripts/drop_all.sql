4df0610724d033b77cd0aca92913d6952e8ce5fe
dabico <dabico@usi.ch>
2022-02-27 16:12:33 +0100

    Updated `drop_all.sql` to quote database names

--- scripts/drop_all.sql
+++ scripts/drop_all.sql
@@ -1,7 +1,7 @@
-drop sequence hibernate_sequence;
-drop table databasechangeloglock;
-drop table databasechangelog;
-drop table function;
-drop table file;
-drop table language;
-drop table git_repo;
+drop table "databasechangeloglock";
+drop table "databasechangelog";
+drop table "function";
+drop table "file";
+drop table "language";
+drop table "git_repo";
+drop sequence "hibernate_sequence";
