a2d519c4a34cd7f874afb3a9998514dcb958f401
dabico <dabico@usi.ch>
2022-03-01 17:34:51 +0100

    Added startup procedure for last crawl job
    If a last job exists it is retrieved, otherwise, it is created.
    This procedure is run only once on startup.

--- dl4se-crawler/src/main/java/usi/si/seart/utils/HibernateUtils.java
+++ dl4se-crawler/src/main/java/usi/si/seart/utils/HibernateUtils.java
@@ -9,9 +9,13 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import usi.si.seart.CrawlerProperties;
 import usi.si.seart.model.GitRepo;
 import usi.si.seart.model.Language;
+import usi.si.seart.model.job.CrawlJob;
+import usi.si.seart.model.job.Job;
 
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -21,6 +25,25 @@ public class HibernateUtils {
 
     @Getter
     private static final SessionFactory factory = new Configuration().configure().buildSessionFactory();
+
+    public static CrawlJob getLastJob() {
+        try (Session session = HibernateUtils.getFactory().openSession()) {
+            Optional<CrawlJob> lastJobOptional = session.createQuery(
+                    "SELECT c FROM CrawlJob c WHERE c.jobType = usi.si.seart.model.job.Job.CODE", CrawlJob.class
+            ).uniqueResultOptional();
+
+            if (lastJobOptional.isPresent()) {
+                return lastJobOptional.get();
+            } else {
+                CrawlJob startJob = CrawlJob.builder()
+                        .checkpoint(CrawlerProperties.startDate.atStartOfDay())
+                        .jobType(Job.CODE)
+                        .build();
+                save(startJob);
+                return getLastJob();
+            }
+        }
+    }
 
     public static Set<Language> getLanguages() {
         try (Session session = HibernateUtils.getFactory().openSession()) {
