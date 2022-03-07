e9a0e3dfb5ecb616fe7f6a2f79421f10fcf862a2
dabico <dabico@usi.ch>
2022-03-07 12:26:41 +0100

    Added set difference to `CollectionUtils`

--- dl4se-crawler/src/main/java/usi/si/seart/collection/utils/CollectionUtils.java
+++ dl4se-crawler/src/main/java/usi/si/seart/collection/utils/CollectionUtils.java
@@ -27,6 +27,22 @@ public class CollectionUtils {
     }
 
     /**
+     * Helper method used for calculating the difference of two sets: a set of items contained in the first set that are
+     * not part of the second set. Although it employs mutable operations, it ensures that the original sets are not
+     * modified by working on their copies.
+     *
+     * @param set1 The first {@link Set}.
+     * @param set2 The second {@link Set}.
+     * @param <T> The type of elements in both sets.
+     * @return The difference of the first and second set.
+     */
+    public <T> Set<T> difference(Set<T> set1, Set<T> set2) {
+        Set<T> difference = new HashSet<>(set1);
+        difference.removeAll(set2);
+        return difference;
+    }
+
+    /**
      * Helper method used for retrieving all values contained in a {@code Map}. If the key does not map to any value,
      * then it is simply ignored. The order of value retrieval depends on the access order of the {@code Collection}
      * containing the keys.
--- dl4se-crawler/src/test/java/usi/si/seart/collection/utils/CollectionUtilsTest.java
+++ dl4se-crawler/src/test/java/usi/si/seart/collection/utils/CollectionUtilsTest.java
@@ -1,31 +1,40 @@
 package usi.si.seart.collection.utils;
 
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 import java.util.Set;
 
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 class CollectionUtilsTest {
+
+    Set<Integer> ints1 = Set.of(1, 2, 3, 4, 5, 6);
+    Set<Integer> ints2 = Set.of(4, 5, 6, 7, 8, 9);
+    Set<String> strs1 = Set.of("this", "is", "a", "utils", "test");
+    Set<String> strs2 = Set.of("this", "is", "another", "test");
+
 
     @Test
     void intersectionTest() {
-        Set<Integer> ints1 = Set.of(1, 2, 3, 4, 5, 6);
-        Set<Integer> ints2 = Set.of(4, 5, 6, 7, 8, 9);
         Assertions.assertEquals(Set.of(4, 5, 6), CollectionUtils.intersection(ints1, ints2));
-
-        Set<String> strs1 = Set.of("this", "is", "a", "intersection", "test");
-        Set<String> strs2 = Set.of("this", "is", "another", "test");
         Assertions.assertEquals(Set.of("this", "is", "test"), CollectionUtils.intersection(strs1, strs2));
+    }
+
+    @Test
+    void differenceTest() {
+        Assertions.assertEquals(Set.of(1, 2, 3), CollectionUtils.difference(ints1, ints2));
+        Assertions.assertEquals(Set.of("a", "utils"), CollectionUtils.difference(strs1, strs2));
     }
 
     @Test
     void getAllKeysFromTest() {
         Map<Integer, Integer> map = Map.of(0, 5, 1, 6, 2, 7, 3, 8, 4, 9);
         Set<Integer> keys = Set.of(0, 1, 2, 3, 4);
-        Set<Integer> values = Set.of(5, 6, 7, 8, 9);
 
-        Assertions.assertEquals(values, CollectionUtils.getAllValuesFrom(map, keys));
+        Assertions.assertEquals(Set.of(5, 6, 7, 8, 9), CollectionUtils.getAllValuesFrom(map, keys));
         Assertions.assertEquals(Set.of(5, 6, 7), CollectionUtils.getAllValuesFrom(map, Set.of(0, 1, 2)));
         Assertions.assertEquals(Set.of(), CollectionUtils.getAllValuesFrom(map, Set.of(5, 6, 7)));
     }
