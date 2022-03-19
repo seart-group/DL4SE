8764bd14aa5e35c0f46a470770d9d53bf25569d4
dabico <dabico@usi.ch>
2022-03-19 17:47:46 +0100

    Added platform skeleton

---
+++ dl4se-platform/src/main/java/usi/si/seart/DL4SEApplication.java
@@ -0,0 +1,11 @@
+package usi.si.seart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DL4SEApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DL4SEApplication.class, args);
+    }
+}
