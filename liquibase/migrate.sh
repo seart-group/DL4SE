4e6362e8984999e5e3271f80b94a49d864427fa5
dabico <dabico@usi.ch>
2022-03-20 22:34:15 +0100

    Altered migration command to use more dedicated parameters

--- liquibase/migrate.sh
+++ liquibase/migrate.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-liquibase --url="jdbc:postgresql://$DATABASE_HOST:$DATABASE_PORT/$DATABASE_NAME?user=$DATABASE_USER&password=$DATABASE_PASS" --changeLogFile="liquibase/changelog.xml" --hub-mode=off update
+liquibase \
+  --username="$DATABASE_USER" \
+  --password="$DATABASE_PASS" \
+  --url="jdbc:postgresql://$DATABASE_HOST:$DATABASE_PORT/$DATABASE_NAME" \
+  --changeLogFile="liquibase/changelog.xml" \
+  --hub-mode=off \
+  update
