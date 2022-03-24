56024c479db83988affa0105ff3a14856f9518ef
dabico <dabico@usi.ch>
2022-03-24 18:17:36 +0100

    Added very relaxed CORS mapping

--- dl4se-platform/src/main/java/usi/si/seart/config/MainConfig.java
+++ dl4se-platform/src/main/java/usi/si/seart/config/MainConfig.java
@@ -3,13 +3,28 @@ package usi.si.seart.config;
 import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
 public class MainConfig {
+
+    @Bean
+    public WebMvcConfigurer webConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull final CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*")
+                        .allowedOrigins("*");
+            }
+        };
+    }
 
     @Bean
     @SneakyThrows
