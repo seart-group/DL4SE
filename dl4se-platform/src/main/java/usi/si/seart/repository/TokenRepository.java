bc3f6890ea90eefb55e4a64e949c925e0e225225
dabico <dabico@usi.ch>
2022-03-28 17:44:07 +0200

    Added `findByValue` method to `TokenRepository`

--- dl4se-platform/src/main/java/usi/si/seart/repository/TokenRepository.java
+++ dl4se-platform/src/main/java/usi/si/seart/repository/TokenRepository.java
@@ -3,5 +3,8 @@ package usi.si.seart.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import usi.si.seart.model.user.token.Token;
 
+import java.util.Optional;
+
 public interface TokenRepository extends JpaRepository<Token, Long> {
+    Optional<Token> findByValue(String value);
 }
