fcc7c5a80396e8acc8e33604d394621b6ae8a2c6
dabico <dabico@usi.ch>
2022-03-28 17:44:53 +0200

    Added User and Verification services

---
+++ dl4se-platform/src/main/java/usi/si/seart/service/UserService.java
@@ -0,0 +1,33 @@
+package usi.si.seart.service;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import usi.si.seart.model.user.Role;
+import usi.si.seart.model.user.User;
+import usi.si.seart.repository.UserRepository;
+
+public interface UserService {
+
+    User register(User user);
+
+    @Service
+    @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+    @RequiredArgsConstructor(onConstructor_ = @Autowired)
+    class UserServiceImpl implements UserService {
+
+        UserRepository userRepository;
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        @Override
+        public User register(User user) {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            user.setRole(Role.USER);
+            return userRepository.save(user);
+        }
+    }
+}
---
+++ dl4se-platform/src/main/java/usi/si/seart/service/VerificationService.java
@@ -0,0 +1,59 @@
+package usi.si.seart.service;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import usi.si.seart.model.user.User;
+import usi.si.seart.model.user.token.Token;
+import usi.si.seart.model.user.token.VerificationToken;
+import usi.si.seart.repository.TokenRepository;
+import usi.si.seart.repository.UserRepository;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Optional;
+
+public interface VerificationService {
+
+    VerificationToken generate(User user);
+    void verify(String tokenValue);
+
+    @Service
+    @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+    @RequiredArgsConstructor(onConstructor_ = @Autowired)
+    class VerificationServiceImpl implements VerificationService {
+
+        TokenRepository tokenRepository;
+        UserRepository userRepository;
+
+        @Override
+        public VerificationToken generate(User user) {
+            VerificationToken token = VerificationToken.builder().user(user).build();
+            return tokenRepository.save(token);
+        }
+
+        @Override
+        public void verify(String tokenValue) {
+            Optional<Token> existing = tokenRepository.findByValue(tokenValue);
+            if (existing.isPresent()) {
+                Token token = existing.get();
+                User user = token.getUser();
+
+                boolean valid = token.getExpires().isAfter(LocalDateTime.now(ZoneOffset.UTC));
+                if (!valid) {
+                    tokenRepository.delete(token);
+                    userRepository.delete(user);
+                    throw new IllegalStateException("Cannot verify user, token has expired!");
+                }
+
+                user.setVerified(true);
+                tokenRepository.delete(token);
+                userRepository.save(user);
+            } else {
+                throw new IllegalArgumentException("Invalid or non-existing token!");
+            }
+        }
+    }
+}
