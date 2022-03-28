7a53753436f9507efd465a5694763669c3c96856
dabico <dabico@usi.ch>
2022-03-28 14:46:55 +0200

    Added DTO for form data sent during account creation

--- dl4se-platform/src/main/java/usi/si/seart/config/MainConfig.java
+++ dl4se-platform/src/main/java/usi/si/seart/config/MainConfig.java
@@ -3,12 +3,14 @@ package usi.si.seart.config;
 import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.lang.NonNull;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import usi.si.seart.converter.DtoToUserConverter;
 
 @Configuration
 @EnableWebSecurity
@@ -22,6 +24,11 @@ public class MainConfig {
                 registry.addMapping("/**")
                         .allowedMethods("*")
                         .allowedOrigins("*");
+            }
+
+            @Override
+            public void addFormatters(@NonNull final FormatterRegistry registry) {
+                registry.addConverter(new DtoToUserConverter());
             }
         };
     }
