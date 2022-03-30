cca64d9ddf92ac8a39b98f3deafddf56e6698658
dabico <dabico@usi.ch>
2022-03-30 16:10:11 +0200

    Split configuration files
    Now we have a `MainConfig` and `SecurityConfig`. The latter configures
    the Authentication Manager and Http Security. It also sets up all the
    security-related beans that will be used throughout the app.

--- dl4se-platform/src/main/java/usi/si/seart/config/MainConfig.java
+++ dl4se-platform/src/main/java/usi/si/seart/config/MainConfig.java
@@ -1,20 +1,14 @@
 package usi.si.seart.config;
 
-import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.lang.NonNull;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import usi.si.seart.converter.DtoToUserConverter;
 
 @Configuration
-@EnableWebSecurity
 public class MainConfig {
 
     @Bean
@@ -32,15 +26,5 @@ public class MainConfig {
                 registry.addConverter(new DtoToUserConverter());
             }
         };
-    }
-
-    @Bean
-    @SneakyThrows
-    public SecurityFilterChain filterChain(HttpSecurity http) {
-        return http.requiresChannel(channel -> channel.anyRequest().requiresSecure())
-                .authorizeRequests(authorize -> authorize.anyRequest().permitAll())
-                .csrf(AbstractHttpConfigurer::disable)
-                .antMatcher("/**")
-                .build();
     }
 }
---
+++ dl4se-platform/src/main/java/usi/si/seart/config/SecurityConfig.java
@@ -0,0 +1,82 @@
+package usi.si.seart.config;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import usi.si.seart.security.CustomUserDetailsService;
+import usi.si.seart.security.jwt.JwtAuthenticationEntryPoint;
+import usi.si.seart.security.jwt.JwtRequestFilter;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true
+)
+@AllArgsConstructor(onConstructor_ = @Autowired)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    CustomUserDetailsService userDetailsService;
+
+    JwtAuthenticationEntryPoint entryPoint;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors()
+                .and()
+                .csrf()
+                .disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(entryPoint)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(
+                        "/user/login",
+                        "/user/logout",
+                        "/user/register",
+                        "/user/verify")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtRequestFilter jwtAuthenticationFilter() {
+        return new JwtRequestFilter();
+    }
+
+    @Override
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
