1b34a96f90e8d514c33d50122ab23561d4205033
dabico <dabico@usi.ch>
2022-03-30 15:44:29 +0200

    Added `findByEmail` method to `UserRepository`

--- dl4se-platform/src/main/java/usi/si/seart/repository/UserRepository.java
+++ dl4se-platform/src/main/java/usi/si/seart/repository/UserRepository.java
@@ -3,5 +3,8 @@ package usi.si.seart.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import usi.si.seart.model.user.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
