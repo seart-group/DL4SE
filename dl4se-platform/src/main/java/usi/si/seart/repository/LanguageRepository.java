0cb8166a767b402054e71adad70ba3deef6ba99d
dabico <dabico@usi.ch>
2022-04-07 15:27:58 +0200

    Added new service and repository for `Language`
    Includes a new `EntityNotFoundException` subtype

---
+++ dl4se-platform/src/main/java/usi/si/seart/repository/LanguageRepository.java
@@ -0,0 +1,11 @@
+package usi.si.seart.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import usi.si.seart.model.Language;
+
+import java.util.Optional;
+
+public interface LanguageRepository extends JpaRepository<Language, Long> {
+
+    Optional<Language> findByName(String name);
+}
---
+++ dl4se-platform/src/main/java/usi/si/seart/service/LanguageService.java
@@ -0,0 +1,29 @@
+package usi.si.seart.service;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import usi.si.seart.exception.LanguageNotFoundException;
+import usi.si.seart.model.Language;
+import usi.si.seart.repository.LanguageRepository;
+
+public interface LanguageService {
+
+    Language getWithName(String name);
+
+    @Service
+    @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+    @AllArgsConstructor(onConstructor_ = @Autowired)
+    class LanguageServiceImpl implements LanguageService {
+
+        LanguageRepository languageRepository;
+
+        @Override
+        public Language getWithName(String name) {
+            return languageRepository.findByName(name)
+                    .orElseThrow(() -> new LanguageNotFoundException("name", name));
+        }
+    }
+}
