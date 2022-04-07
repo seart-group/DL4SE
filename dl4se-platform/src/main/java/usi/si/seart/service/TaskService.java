743d1085f8c4ac22afc33610a9ac9321fe947889
dabico <dabico@usi.ch>
2022-04-07 15:39:15 +0200

    Added `TaskService`
    Contains procedures for creating new tasks, as well as verifying if a
    user has free slots for creating new tasks. For now, the task limit is
    hard-coded into the application.properties file.

---
+++ dl4se-platform/src/main/java/usi/si/seart/service/TaskService.java
@@ -0,0 +1,65 @@
+package usi.si.seart.service;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import usi.si.seart.model.task.Task;
+import usi.si.seart.model.task.processing.Processing;
+import usi.si.seart.model.task.query.Query;
+import usi.si.seart.model.user.User;
+import usi.si.seart.repository.TaskRepository;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public interface TaskService {
+
+    boolean canCreateTask(User user);
+    void create(User requester, LocalDateTime requestedAt, Query query, Processing processing);
+
+    @Service
+    @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+    @RequiredArgsConstructor(onConstructor_ = @Autowired)
+    class TaskServiceImpl implements TaskService {
+
+        Path fileStorageDirPath;
+        TaskRepository taskRepository;
+
+        @NonFinal
+        @Value("${app.request.limit}")
+        Long userTaskLimit;
+
+        @Override
+        public boolean canCreateTask(User user) {
+            Long activeTasks = taskRepository.countActiveTasksByUser(user);
+            return activeTasks < userTaskLimit;
+        }
+
+        @Override
+        @SneakyThrows
+        public void create(User requester, LocalDateTime requestedAt, Query query, Processing processing) {
+            UUID uuid = UUID.randomUUID();
+            String prefix = requester.getId() + "_" + uuid + "_";
+            Path requestFile = Files.createTempFile(fileStorageDirPath, prefix, ".jsonl");
+            Task task = Task.builder()
+                    .uuid(uuid)
+                    .user(requester)
+                    .submitted(requestedAt)
+                    .exportPath(requestFile.toString())
+                    .query(query)
+                    .processing(processing)
+                    .build();
+
+            query.setTask(task);
+            processing.setTask(task);
+            taskRepository.save(task);
+        }
+    }
+}
