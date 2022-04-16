4ffa4f4c52fbbca5e688d3d596c242fa3ebf79e8
dabico <dabico@usi.ch>
2022-04-16 11:23:23 +0200

    Added `CodeService`
    This service streams the results of the specified `query`, all the while
    applying the processing operation to the returned `Stream`

---
+++ dl4se-platform/src/main/java/usi/si/seart/service/CodeService.java
@@ -0,0 +1,43 @@
+package usi.si.seart.service;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.jooq.DSLContext;
+import org.jooq.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import usi.si.seart.model.code.Code;
+import usi.si.seart.repository.CodeRepository;
+
+import java.util.Map;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public interface CodeService {
+
+    <T extends Code> Stream<Code> createPipeline(Query query, UnaryOperator<Code> pipeline, Class<T> codeClass);
+
+    @Service
+    @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+    @AllArgsConstructor(onConstructor_ = @Autowired)
+    class CodeServiceImpl implements CodeService {
+
+        DSLContext dslContext;
+        CodeRepository codeRepository;
+
+        @SuppressWarnings("ConstantConditions")
+        public <T extends Code> Stream<Code> createPipeline(
+                Query query, UnaryOperator<Code> processing, Class<T> codeClass
+        ) {
+            String sql = dslContext.renderNamedParams(query);
+            Map<String, ?> parameters = query.getParams()
+                    .entrySet()
+                    .stream()
+                    .map(entry -> Map.entry(entry.getKey(), entry.getValue().getValue()))
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+            return codeRepository.stream(sql, parameters, codeClass).map(processing);
+        }
+    }
+}
