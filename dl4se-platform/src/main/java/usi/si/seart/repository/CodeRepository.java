aabf1dfca1bf17cc280685a891985f0aaffc15a2
dabico <dabico@usi.ch>
2022-04-17 22:05:30 +0200

    Added `CodeRepository` method for counting the query results
    This will be run prior to the task being executed. We do this to
    "estimate" the total number of results that will be processed. I say
    "estimate" because we do not execute an actual count query. Instead, we
    get the row number of the last result set element from a scrollable
    result set of the regular query.

--- dl4se-platform/src/main/java/usi/si/seart/repository/CodeRepository.java
+++ dl4se-platform/src/main/java/usi/si/seart/repository/CodeRepository.java
@@ -1,5 +1,9 @@
 package usi.si.seart.repository;
 
+import lombok.Cleanup;
+import org.hibernate.ScrollableResults;
+import org.hibernate.Session;
+import org.hibernate.query.NativeQuery;
 import org.springframework.stereotype.Repository;
 import usi.si.seart.model.code.Code;
 
@@ -12,6 +16,7 @@ import java.util.stream.Stream;
 public interface CodeRepository {
 
     <T extends Code> Stream<T> stream(String queryString, Map<String, ?> parameters, Class<T> codeClass);
+    <T extends Code> Long count(String queryString, Map<String, ?> parameters, Class<T> codeClass);
 
     @Repository
     class CodeRepositoryImpl implements CodeRepository {
@@ -27,6 +32,20 @@ public interface CodeRepository {
             Query query = entityManager.createNativeQuery(queryString, codeClass);
             parameters.forEach(query::setParameter);
             return query.getResultStream();
+        }
+
+        @Override
+        public <T extends Code> Long count(String queryString, Map<String, ?> parameters, Class<T> codeClass) {
+            @Cleanup Session session = entityManager.unwrap(Session.class);
+            NativeQuery<T> query = session.createNativeQuery(queryString, codeClass)
+                    .setReadOnly(true)
+                    .setCacheable(false);
+            parameters.forEach(query::setParameter);
+            @Cleanup ScrollableResults scroll = query.scroll();
+            long results = 0L;
+            if (scroll.last())
+                results = scroll.getRowNumber() + 1L;
+            return results;
         }
     }
 }
