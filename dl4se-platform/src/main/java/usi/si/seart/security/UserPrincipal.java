4f3fa359253f0bb15bf3e894e956dbfb820f6cc9
dabico <dabico@usi.ch>
2022-04-20 23:52:50 +0200

    Appending "ROLE_" prefix when constructing SimpleGrantedAuthority
    Not doing this was causing a 403 when checking "hasRole('ADMIN')"

--- dl4se-platform/src/main/java/usi/si/seart/security/UserPrincipal.java
+++ dl4se-platform/src/main/java/usi/si/seart/security/UserPrincipal.java
@@ -47,7 +47,7 @@ public class UserPrincipal implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
     }
 
     public String getEmail() {
