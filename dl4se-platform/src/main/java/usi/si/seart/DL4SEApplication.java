7eceae303d9063b2c0c255c8c67f51afa5393868
dabico <dabico@usi.ch>
2022-04-25 15:09:45 +0200

    Simplified the `PostConstruct` procedure of the main class
    Now we simply suppress the IOException, rather than swallowing it and
    suffering the consequences later. The exception in question is only
    thrown if we can not create the temporary directory for the platform on
    the filesystem. In such a case, the server should not be running anyway,
    as all the components that rely on the `FileSystemService` are going to
    experience failures.

--- dl4se-platform/src/main/java/usi/si/seart/DL4SEApplication.java
+++ dl4se-platform/src/main/java/usi/si/seart/DL4SEApplication.java
@@ -1,13 +1,13 @@
 package usi.si.seart;
 
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -22,16 +22,12 @@ public class DL4SEApplication {
         SpringApplication.run(DL4SEApplication.class, args);
     }
 
+    @SneakyThrows
     @PostConstruct
     private void createFileStorageDirectory() {
         if (Files.notExists(fileStorageDirPath)) {
             log.info("File storage directory: '{}' not found! Initializing new one...", fileStorageDirPath);
-            try {
-                Files.createDirectory(fileStorageDirPath);
-            } catch (IOException ex) {
-                log.error("Could not create file storage directory!", ex);
-            }
-            log.info("Successfully initialized file storage directory!");
+            Files.createDirectory(fileStorageDirPath);
         } else {
             log.info("File storage directory: '{}'", fileStorageDirPath);
         }
