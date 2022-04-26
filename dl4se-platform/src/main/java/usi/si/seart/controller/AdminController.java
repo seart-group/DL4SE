c72e3d462ae7a2f45c8042c06ce7a2e565028663
dabico <dabico@usi.ch>
2022-04-26 18:32:05 +0200

    Added new annotation for securing controllers
    The `AdminRestController` annotation serves as a shorthand for:
    1) `@Secured("ROLE_ADMIN")`
    2) `@RestController`

--- dl4se-platform/src/main/java/usi/si/seart/controller/AdminController.java
+++ dl4se-platform/src/main/java/usi/si/seart/controller/AdminController.java
@@ -7,23 +7,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import usi.si.seart.dto.ConfigurationDto;
 import usi.si.seart.model.Configuration;
 import usi.si.seart.model.user.User;
+import usi.si.seart.security.annotation.AdminRestController;
 import usi.si.seart.service.ConfigurationService;
 import usi.si.seart.service.UserService;
 
 import javax.validation.Valid;
 
-@RestController
+@AdminRestController
 @RequestMapping("/admin")
-@PreAuthorize("hasRole('ADMIN')")
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @AllArgsConstructor(onConstructor_ = @Autowired)
 public class AdminController {
