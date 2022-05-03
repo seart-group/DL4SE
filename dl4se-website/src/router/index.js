f969b9e8b2c80a5e569e3f161985610cbe0faaf2
dabico <dabico@usi.ch>
2022-05-03 23:43:51 +0200

    Removing lazy-loaded routing for now

--- dl4se-website/src/router/index.js
+++ dl4se-website/src/router/index.js
@@ -1,5 +1,6 @@
 import {createRouter, createWebHistory} from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
+import AboutView from '@/views/AboutView.vue'
 
 const routes = [
   {
@@ -10,10 +11,7 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: AboutView
   }
 ]
 
