fd0f699b710048f2637d19e649630dce1a9cae86
dabico <dabico@usi.ch>
2022-05-03 23:44:20 +0200

    Removed `HelloWorld.vue`

--- dl4se-website/src/views/HomeView.vue
+++ dl4se-website/src/views/HomeView.vue
@@ -1,18 +1,5 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Welcome to Your Vue.js App</h1>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
