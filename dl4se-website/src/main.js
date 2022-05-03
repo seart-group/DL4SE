c676dd65751703930bad7f34cb807d8291c48d12
dabico <dabico@usi.ch>
2022-05-03 23:42:50 +0200

    Tidying up `main.js`

--- dl4se-website/src/main.js
+++ dl4se-website/src/main.js
@@ -1,6 +1,9 @@
 import {createApp} from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import App from '@/App'
+import router from '@/router'
+import store from '@/store'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(store)
+app.mount('#app')
