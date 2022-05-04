10eaf5204193b75b9cbf5a94c432b568758ffbb7
dabico <dabico@usi.ch>
2022-05-05 01:07:07 +0200

    Added store variable `token` with dedicated set/clear operations

--- dl4se-website/src/store/index.js
+++ dl4se-website/src/store/index.js
@@ -2,10 +2,17 @@ import {createStore} from 'vuex'
 
 export default createStore({
   state: {
+    token: ""
   },
   getters: {
   },
   mutations: {
+    setToken(state, value) {
+      state.token = value
+    },
+    clearToken(state) {
+      state.token = ""
+    }
   },
   actions: {
   },
