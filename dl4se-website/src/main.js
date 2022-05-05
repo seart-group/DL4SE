561ae89aae070c34ab0ff1604fd08d186877bbb7
dabico <dabico@usi.ch>
2022-05-06 01:03:37 +0200

    Added Bootstrap Vue Icons to project
    We can now use dedicated icon components in the application

--- dl4se-website/src/main.js
+++ dl4se-website/src/main.js
@@ -4,7 +4,7 @@ import router from '@/router'
 import store from '@/store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import BootstrapVue from 'bootstrap-vue'
+import {BootstrapVue, BootstrapVueIcons} from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
@@ -12,6 +12,7 @@ Vue.config.productionTip = false
 
 Vue.use(VueAxios, axios)
 Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
 
 new Vue({
   router,
