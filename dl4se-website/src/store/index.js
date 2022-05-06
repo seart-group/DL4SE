9839eb17fd4e684d40c8c7c13500f193ecf32fd1
dabico <dabico@usi.ch>
2022-05-06 13:13:04 +0200

    Authentication token is now preserved for page reloads.
    Using the vuex-persist plugin, we store the retrieved token during login
    so that it may be used in other parts of the application. Pages such as
    `login`, `profile` and the task creation page should perform checks on
    the value of the token by making API calls during page load. This way,
    if a token has expired it can be cleared, forcing the user to log back
    in once more.

--- dl4se-website/src/store/index.js
+++ dl4se-website/src/store/index.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
 
 Vue.use(Vuex)
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
 
 export default new Vuex.Store({
   state: {
@@ -20,5 +25,6 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
-  }
+  },
+  plugins: [ vuexLocal.plugin ]
 })
