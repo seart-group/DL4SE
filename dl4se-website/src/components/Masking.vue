59ec85005425cc475aacddd980e697fb75071dd2
dabico <dabico@usi.ch>
2022-06-01 17:00:29 +0200

    Added `Masking` component

---
+++ dl4se-website/src/components/Masking.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="masking">
+    <label :for="id + '-counter'" class="m-0">
+      Randomly mask&nbsp;
+    </label>
+    <b-counter :id="id + '-counter'"
+               class="py-2" counter-class="masking-counter-input"
+               :min="1" :max="100" placeholder="%"
+               v-model.number="local.masking.percentage"
+    />
+    <p class="m-0">
+      &nbsp;of tokens&nbsp;
+    </p>
+    <label :for="id + '-mask'" class="m-0">
+      using the&nbsp;
+    </label>
+    <div class="py-2">
+      <b-input :id="id + '-mask'" class="masking-token-input"
+               placeholder="<MASK>"
+               v-model="local.masking.token"
+      />
+    </div>
+    <p class="m-0">
+      &nbsp;token
+    </p>
+  </div>
+</template>
+
+<script>
+import BCounter from "@/components/Counter";
+
+export default {
+  name: "b-masking",
+  components: { BCounter },
+  props: {
+    id: String,
+    masking: Object
+  },
+  watch: {
+    "local.masking": function () {
+      this.$emit("update:masking", this.local.masking)
+    }
+  },
+  data() {
+    return {
+      local: {
+        masking: this.masking
+      }
+    }
+  }
+}
+</script>
