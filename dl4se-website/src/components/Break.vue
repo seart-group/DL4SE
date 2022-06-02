b23b4188b135e622b0b9afaa16a7dab304db0f57
dabico <dabico@usi.ch>
2022-06-02 11:12:59 +0200

    Added `Break` functional component
    Will be used in other components as a manual DOM break. The display of
    the 100% width `div` used for this component uses bootstraps `d-XX-none`
    class to stop applying the break at specific widths.

---
+++ dl4se-website/src/components/Break.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: "b-break",
+  functional: true,
+  props: {
+    xl: Boolean,
+    lg: Boolean,
+    md: Boolean,
+    sm: Boolean
+  },
+  render(createElement, { props }) {
+    const classes = {
+      'w-100': true,
+      'd-xl-none': props.xl,
+      'd-lg-none': props.lg,
+      'd-md-none': props.md,
+      'd-sm-none': props.sm,
+      'd-none': !Object.values(props).reduce((curr, acc) => acc || curr, false)
+    }
+    return createElement('div', { class: classes })
+  }
+}
+</script>
