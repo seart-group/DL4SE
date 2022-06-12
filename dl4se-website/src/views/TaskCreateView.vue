0848ec55b4a638ac980eda998507722816c55275
dabico <dabico@usi.ch>
2022-06-13 00:04:25 +0200

    Refactored `TaskCreateView`
    Now the object housed in `data` matches the format expected by the
    back-end, requiring no additional transformations before being sent.

--- dl4se-website/src/views/TaskCreateView.vue
+++ dl4se-website/src/views/TaskCreateView.vue
@@ -1,14 +1,22 @@
 <template>
   <div id="task">
     <h1 class="page-title">Specify your dataset</h1>
-    <b-section-repo class="task-form-section-top" :options="options.languages"
-                    :language="language" @update:language="language = $event"
-                    :has-license="has_license" @update:has-license="has_license = $event"
-                    :exclude="exclude" @update:exclude="exclude = $event"
-                    :commits="commits" @update:commits="commits = $event"
-                    :contributors="contributors" @update:contributors="contributors = $event"
-                    :issues="issues" @update:issues="issues = $event"
-                    :stars="stars" @update:stars="stars = $event"
+    <b-section-repo class="task-form-section-top"
+                    :options="options.languages"
+                    :language="task.query.language_name"
+                    @update:language="task.query.language_name = $event"
+                    :has-license="task.query.has_license"
+                    @update:has-license="task.query.has_license = $event"
+                    :commits="{ lower: task.query.min_commits }"
+                    @update:commits="task.query.min_commits = $event.lower"
+                    :contributors="{ lower: task.query.min_contributors }"
+                    @update:contributors="task.query.min_contributors = $event.lower"
+                    :issues="{ lower: task.query.min_issues }"
+                    @update:issues="task.query.min_issues = $event.lower"
+                    :stars="{ lower: task.query.min_stars }"
+                    @update:stars="task.query.min_stars = $event.lower"
+                    :exclude="{ forks: task.query.exclude_forks }"
+                    @update:exclude="task.query.exclude_forks = $event.forks"
     />
     <b-container class="py-4 task-form-section-middle">
       <b-row>
@@ -19,21 +27,49 @@
       <b-row>
         <b-col>
           <b-form-group label="Each instance is a:" label-class="font-weight-bold" class="m-0">
-            <b-form-radio-group id="type-radio" required v-model="type" :options="options.types" />
+            <b-form-radio-group id="type-radio" required v-model="task.query.type" :options="options.types" />
           </b-form-group>
         </b-col>
       </b-row>
     </b-container>
-    <b-section-filters-code class="pb-4 task-form-section-middle" :granularity="type"
-                            :exclude="exclude" @update:exclude="exclude = $event"
-                            :characters="characters" @update:characters="characters = $event"
-                            :tokens="tokens" @update:tokens="tokens = $event"
-                            :lines="lines" @update:lines="lines = $event"
+    <b-section-filters-code class="pb-4 task-form-section-middle" :granularity="task.query.type"
+                            :characters="{ lower: task.query.min_characters, upper: task.query.max_characters }"
+                            @update:characters="task.query.min_characters = $event.lower
+                                                task.query.max_characters = $event.upper"
+                            :tokens="{ lower: task.query.min_tokens, upper: task.query.max_tokens }"
+                            @update:tokens="task.query.min_tokens = $event.lower
+                                            task.query.max_tokens = $event.upper"
+                            :lines="{ lower: task.query.min_lines, upper: task.query.max_lines }"
+                            @update:lines="task.query.min_lines = $event.lower
+                                           task.query.max_lines = $event.upper"
+                            :exclude="{
+                              test: task.query.exclude_test,
+                              boilerplate: task.query.exclude_boilerplate,
+                              unparsable: task.query.exclude_unparsable,
+                              nonAscii: task.query.exclude_non_ascii,
+                              duplicates: task.query.exclude_duplicates,
+                              identical: task.query.exclude_identical
+                            }" @update:exclude="task.query.exclude_test = $event.test
+                                                task.query.exclude_boilerplate = $event.boilerplate
+                                                task.query.exclude_unparsable = $event.unparsable
+                                                task.query.exclude_non_ascii = $event.nonAscii
+                                                task.query.exclude_duplicates = $event.duplicates
+                                                task.query.exclude_identical = $event.identical"
     />
     <b-section-processing-code class="task-form-section-middle"
-                               :remove="remove" @update:remove="remove = $event"
-                               :masking="masking" @update:masking="masking = $event"
-                               :idioms="idioms" @update:idioms="idioms = $event"
+                               :remove="{
+                                 docstring: task.processing.remove_docstring,
+                                 innerComments: task.processing.remove_inner_comments
+                               }" @update:remove="task.processing.remove_docstring = $event.docstring
+                                                  task.processing.remove_inner_comments = $event.innerComments"
+                               :masking="{
+                                 token: task.processing.mask_token,
+                                 percentage: task.processing.mask_percentage,
+                                 contiguousOnly: task.processing.mask_contiguous_only
+                               }" @update:masking="task.processing.mask_token = $event.token
+                                                   task.processing.mask_percentage = $event.percentage
+                                                   task.processing.mask_contiguous_only = $event.contiguousOnly"
+                               :idioms="task.processing.idioms" @update:idioms="task.processing.idioms = $event"
     />
     <b-container class="py-4 task-form-section-bottom">
       <b-row align-h="center">
@@ -65,49 +101,8 @@ export default {
   },
   methods: {
     async submit() {
-      const payload = {
-        query: {},
-        processing: {}
-      }
-
-      payload.query["type"] = this.type
-      payload.query["language_name"] = this.language
-      payload.query["has_license"] = this.has_license
-      payload.query["exclude_forks"] = this.exclude.forks
-      payload.query["min_commits"] = this.commits.lower || 0
-      payload.query["min_contributors"] = this.contributors.lower || 0
-      payload.query["min_issues"] = this.issues.lower || 0
-      payload.query["min_stars"] = this.stars.lower || 0
-      payload.query["include_ast"] = this.include.ast
-      payload.query["min_tokens"] = this.tokens.lower
-      payload.query["max_tokens"] = this.tokens.upper
-      payload.query["min_lines"] = this.lines.lower
-      payload.query["max_lines"] = this.lines.upper
-      payload.query["min_characters"] = this.characters.lower
-      payload.query["max_characters"] = this.characters.upper
-      payload.query["exclude_duplicates"] = this.exclude.duplicates
-      payload.query["exclude_identical"] = this.exclude.identical
-      payload.query["exclude_test"] = this.exclude.test
-      payload.query["exclude_non_ascii"] = this.exclude.non_ascii
-      if (this.type === "file") {
-        payload.query["exclude_unparsable"] = this.exclude.unparsable
-      } else {
-        payload.query["exclude_boilerplate"] = this.exclude.boilerplate
-      }
-
-      payload.processing["remove_docstring"] = this.remove.docstring
-      payload.processing["remove_inner_comments"] = this.remove.inner_comments
-      payload.processing["mask_token"] = this.masking.token
-      payload.processing["mask_percentage"] = this.masking.percentage
-      payload.processing["mask_contiguous_only"] = this.masking.contiguous_only
-      payload.processing["idioms"] = this.idioms
-
-      const config = {
-        headers : {
-          'authorization': this.$store.getters.getToken
-        }
-      }
-
+      const payload = this.task
+      const config = { headers : { 'authorization': this.$store.getters.getToken } }
       await axios.post("https://localhost:8080/api/task/create", payload, config)
     }
   },
@@ -118,6 +113,39 @@ export default {
   },
   data() {
     return {
+      task: {
+        query: {
+          type : "file",
+          language_name: null,
+          has_license : false,
+          min_commits: null,
+          min_contributors: null,
+          min_issues: null,
+          min_stars: null,
+          min_tokens: null,
+          max_tokens: null,
+          min_lines: null,
+          max_lines: null,
+          min_characters: null,
+          max_characters: null,
+          include_ast: true,
+          exclude_forks: false,
+          exclude_duplicates: false,
+          exclude_identical: false,
+          exclude_test: false,
+          exclude_non_ascii: false,
+          exclude_unparsable: false,
+          exclude_boilerplate: false
+        },
+        processing: {
+          remove_docstring: false,
+          remove_inner_comments: false,
+          mask_token: null,
+          mask_percentage: null,
+          mask_contiguous_only: null,
+          idioms: []
+        }
+      },
       options: {
         types: [
           {
@@ -130,60 +158,7 @@ export default {
           }
         ],
         languages: ['Java', 'Python', 'C++'],
-      },
-      type : "file",
-      language: null,
-      has_license : false,
-      commits: {
-        lower: null,
-        upper: null
-      },
-      contributors: {
-        lower: null,
-        upper: null
-      },
-      issues: {
-        lower: null,
-        upper: null
-      },
-      stars: {
-        lower: null,
-        upper: null
-      },
-      characters: {
-        lower: null,
-        upper: null
-      },
-      tokens: {
-        lower: null,
-        upper: null
-      },
-      lines: {
-        lower: null,
-        upper: null
-      },
-      exclude: {
-        forks: false,
-        identical : false,
-        duplicates: false,
-        test : false,
-        non_ascii : false,
-        boilerplate : false,
-        unparsable: false
-      },
-      include: {
-        ast: true
-      },
-      masking: {
-        token: null,
-        percentage: null,
-        contiguous_only : null
-      },
-      remove: {
-        docstring: false,
-        inner_comments: false
-      },
-      idioms: []
+      }
     }
   }
 }
