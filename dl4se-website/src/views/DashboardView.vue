f2fb10d3f6c9680b816cede9eb2119d88cfd46e9
dabico <dabico@usi.ch>
2022-06-14 21:20:44 +0200

    Starting work on table

--- dl4se-website/src/views/DashboardView.vue
+++ dl4se-website/src/views/DashboardView.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="dashboard">
-    <b-button to="task" class="action-btn">Create Task</b-button>
+    <b-container>
+      <b-row>
+        <b-col cols="auto">
+          <!-- TODO 14.06.22: Table goes here -->
+        </b-col>
+      </b-row>
+      <b-row align-h="center">
+        <b-col cols="auto">
+          <b-button to="task" class="action-btn">Create Task</b-button>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
