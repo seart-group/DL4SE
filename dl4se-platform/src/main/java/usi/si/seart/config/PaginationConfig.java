65485847da30852b41aeec933f9468dadd217a7f
dabico <dabico@usi.ch>
2022-06-20 17:33:39 +0200

    Added pagination config overrides
    Rather than explicitly declaring paging parameters, we will use a
    `Pageable` object in the request method parameter to handle the `page`,
    `size` and `sort` request parameter values. At the same time, we use the
    newly introduced `max_page_size` to override Spring's default 2000
    maximum page size default setting. Note that although the
    `max_page_size` is changeable, changes made to it will only take effect
    on restart.

---
+++ dl4se-platform/src/main/java/usi/si/seart/config/PaginationConfig.java
@@ -0,0 +1,23 @@
+package usi.si.seart.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer;
+import usi.si.seart.service.ConfigurationService;
+
+@Configuration
+@EnableSpringDataWebSupport
+@AllArgsConstructor(onConstructor_ = @Autowired)
+public class PaginationConfig {
+
+    ConfigurationService configurationService;
+
+    @Bean
+    PageableHandlerMethodArgumentResolverCustomizer pageableResolverCustomizer() {
+        Integer maxPageSize = configurationService.get("max_page_size", Integer.class);
+        return pageableResolver -> pageableResolver.setMaxPageSize(maxPageSize);
+    }
+}
