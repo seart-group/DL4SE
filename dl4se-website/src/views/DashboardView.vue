b2314b8d36bbae15d81feb01c8d2b4a8319bf0fc
dabico <dabico@usi.ch>
2022-06-23 23:43:32 +0200

    Added user table to `DashboardView`

--- dl4se-website/src/views/DashboardView.vue
+++ dl4se-website/src/views/DashboardView.vue
@@ -2,10 +2,50 @@
   <div id="dashboard">
     <b-container>
       <b-row>
-        <b-col cols="auto">
-          <!-- TODO 14.06.22: Table goes here -->
+        <b-col>
+          <h3 class="mb-3">Platform Users</h3>
         </b-col>
       </b-row>
+      <b-row align-h="center">
+        <b-col>
+          <b-paginated-table id="user-table" api-url="https://localhost:8080/api/admin/user"
+                             :fields="userTable.fields" :primary-key="userTable.fields[0].key"
+          >
+            <template #cell(registered)="row">
+              <span :title="row.unformatted">{{ row.value }}</span>
+            </template>
+            <template #cell(details)="row">
+              <b-icon-patch-check-fill v-if="row.item.verified" title="Email Verified" class="mr-1" />
+              <b-icon-patch-question-fill v-else title="Email Unverified" class="mr-1" />
+              <b-iconstack :title="(row.item.enabled ? '' : 'Disabled ') + toTitleCase(row.item.role)">
+                <b-icon-person-plus-fill stacked v-if="row.item.role === 'ADMIN'" shift-h="2" />
+                <b-icon-person-fill stacked v-else />
+                <b-icon-x-circle v-if="!row.item.enabled" stacked variant="danger" />
+              </b-iconstack>
+            </template>
+            <template #cell(actions)="row">
+              <div class="d-lg-table-cell d-inline-flex">
+                <b-button class="action-btn mr-1" size="sm"
+                          :title="!row.item.enabled ? 'Enable' : 'Disable'"
+                          @click="userAction(row.item.uid, !row.item.enabled ? 'enable' : 'disable')"
+                >
+                  <b-icon-person-check-fill v-if="!row.item.enabled" />
+                  <b-icon-person-x-fill v-else />
+                </b-button>
+                <b-button class="action-btn" size="sm"
+                          :title="row.item.role === 'ADMIN' ? 'Demote' : 'Promote'"
+                          @click="userAction(row.item.uid, row.item.role === 'ADMIN' ? 'demote' : 'promote')"
+                >
+                  <b-icon-person-dash-fill v-if="row.item.role === 'ADMIN'" />
+                  <b-icon-person-plus-fill v-else />
+                </b-button>
+              </div>
+            </template>
+          </b-paginated-table>
+        </b-col>
+      </b-row>
+    </b-container>
+    <b-container>
       <b-row align-h="center">
         <b-col cols="auto">
           <b-button to="task" class="action-btn">Create Task</b-button>
@@ -16,6 +56,54 @@
 </template>
 
 <script>
+import axios from "axios"
+import BPaginatedTable from "@/components/PaginatedTable"
+
 export default {
+  components: { BPaginatedTable },
+  methods: {
+    toTitleCase(str) {
+      return str.replace(
+          /\w\S*/g,
+          (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+      )
+    },
+    async userAction(uid, action) {
+      const url = `https://localhost:8080/api/admin/user/${uid}/${action}`
+      const config = {
+        headers: { 'authorization': this.$store.getters.getToken }
+      }
+      await axios.post(url, null, config).then(() => {
+        this.$root.$emit("bv::refresh::table", "user-table")
+      }).catch(console.log)
+      // TODO 23.06.22: Better error handling
+    }
+  },
+  data() {
+    return {
+      userTable: {
+        fields: [
+          {
+            key: "uid",
+            label: "UID",
+            sortable: true
+          },
+          {
+            key: "email",
+            sortable: true
+          },
+          {
+            key: "organisation",
+            sortable: true
+          },
+          {
+            key: "registered",
+            sortable: true,
+            formatter: (value) => value.split('T')[0]
+          }
+        ]
+      }
+    }
+  }
 }
 </script>
