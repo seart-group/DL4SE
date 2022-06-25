e39b78e683830c47f4408d683800dbb88ae2ab14
dabico <dabico@usi.ch>
2022-06-25 02:00:07 +0200

    Replaced plain html titles with bootstrap vue tooltip directives

--- dl4se-website/src/views/DashboardView.vue
+++ dl4se-website/src/views/DashboardView.vue
@@ -18,9 +18,9 @@
               <span :title="row.unformatted">{{ row.value }}</span>
             </template>
             <template #cell(details)="row">
-              <b-icon-patch-check-fill v-if="row.item.verified" title="Email Verified" class="mr-1" />
-              <b-icon-patch-question-fill v-else title="Email Unverified" class="mr-1" />
-              <b-iconstack :title="(row.item.enabled ? '' : 'Disabled ') + toTitleCase(row.item.role)">
+              <b-icon-patch-check-fill v-if="row.item.verified" v-b-tooltip="'Email Verified'" class="mr-1" />
+              <b-icon-patch-question-fill v-else v-b-tooltip="'Email Unverified'" class="mr-1" />
+              <b-iconstack v-b-tooltip="(row.item.enabled ? '' : 'Disabled ') + toTitleCase(row.item.role)">
                 <b-icon-person-plus-fill stacked v-if="row.item.role === 'ADMIN'" shift-h="2" />
                 <b-icon-person-fill stacked v-else />
                 <b-icon-x-circle v-if="!row.item.enabled" stacked variant="danger" />
@@ -29,14 +29,14 @@
             <template #cell(actions)="row">
               <div class="d-lg-table-cell d-inline-flex">
                 <b-button class="action-btn mr-1" size="sm"
-                          :title="!row.item.enabled ? 'Enable' : 'Disable'"
+                          v-b-tooltip="!row.item.enabled ? 'Enable' : 'Disable'"
                           @click="userAction(row.item.uid, !row.item.enabled ? 'enable' : 'disable')"
                 >
                   <b-icon-person-check-fill v-if="!row.item.enabled" />
                   <b-icon-person-x-fill v-else />
                 </b-button>
                 <b-button class="action-btn" size="sm"
-                          :title="row.item.role === 'ADMIN' ? 'Demote' : 'Promote'"
+                          v-b-tooltip="row.item.role === 'ADMIN' ? 'Demote' : 'Promote'"
                           @click="userAction(row.item.uid, row.item.role === 'ADMIN' ? 'demote' : 'promote')"
                 >
                   <b-icon-person-dash-fill v-if="row.item.role === 'ADMIN'" />
