3943502a6b10351b87e17f1a5b853594dcfafdc2
dabico <dabico@usi.ch>
2022-06-26 15:21:47 +0200

    Compacted icon generation into a single tags
    Rather than using multiple tags with `v-if` directives, we compact the
    code into singular HTML tags and use ternary operators to determine
    the type of icon used.

--- dl4se-website/src/views/DashboardView.vue
+++ dl4se-website/src/views/DashboardView.vue
@@ -18,29 +18,33 @@
               <span v-b-tooltip="row.value.toISOString()">{{ row.value.toISOString().split('T')[0] }}</span>
             </template>
             <template #cell(details)="row">
-              <b-icon-patch-check-fill v-if="row.item.verified" v-b-tooltip="'Email Verified'" class="mr-1" />
-              <b-icon-patch-question-fill v-else v-b-tooltip="'Email Unverified'" class="mr-1" />
+              <b-icon :icon="(row.item.verified) ? 'patch-check-fill' : 'patch-question-fill'"
+                      v-b-tooltip="(row.item.verified) ? 'Email Verified' : 'Email Unverified'"
+                      class="mr-1"
+              />
               <b-iconstack v-b-tooltip="(row.item.enabled ? '' : 'Disabled ') + toTitleCase(row.item.role)">
-                <b-icon-person-plus-fill stacked v-if="row.item.role === 'ADMIN'" shift-h="2" />
-                <b-icon-person-fill stacked v-else />
-                <b-icon-x-circle v-if="!row.item.enabled" stacked variant="danger" />
+                <b-icon :icon="(row.item.role === 'ADMIN') ? 'person-plus-fill' : 'person-fill'"
+                        :shift-h="(row.item.role === 'ADMIN') ? 2 : 0"
+                        stacked
+                />
+                <b-icon icon="x-circle" stacked variant="danger"
+                        v-if="!row.item.enabled"
+                />
               </b-iconstack>
             </template>
             <template #cell(actions)="row">
               <div class="d-lg-table-cell d-inline-flex">
                 <b-button class="action-btn mr-1" size="sm"
-                          v-b-tooltip="!row.item.enabled ? 'Enable' : 'Disable'"
-                          @click="userAction(row.item.uid, !row.item.enabled ? 'enable' : 'disable')"
+                          v-b-tooltip="(!row.item.enabled) ? 'Enable' : 'Disable'"
+                          @click="userAction(row.item.uid, (!row.item.enabled) ? 'enable' : 'disable')"
                 >
-                  <b-icon-person-check-fill v-if="!row.item.enabled" />
-                  <b-icon-person-x-fill v-else />
+                  <b-icon :icon="(!row.item.enabled) ? 'person-check-fill' : 'person-x-fill'" />
                 </b-button>
                 <b-button class="action-btn" size="sm"
-                          v-b-tooltip="row.item.role === 'ADMIN' ? 'Demote' : 'Promote'"
-                          @click="userAction(row.item.uid, row.item.role === 'ADMIN' ? 'demote' : 'promote')"
+                          v-b-tooltip="(row.item.role === 'ADMIN') ? 'Demote' : 'Promote'"
+                          @click="userAction(row.item.uid, (row.item.role === 'ADMIN') ? 'demote' : 'promote')"
                 >
-                  <b-icon-person-dash-fill v-if="row.item.role === 'ADMIN'" />
-                  <b-icon-person-plus-fill v-else />
+                  <b-icon :icon="(row.item.role === 'ADMIN') ? 'person-dash-fill' : 'person-plus-fill'" />
                 </b-button>
               </div>
             </template>
