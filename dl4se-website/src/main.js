c6788b33d6b65de19bc89d2375d18514640dc42c
dabico <dabico@usi.ch>
2022-06-28 16:27:17 +0200

    Added `vue-fragment` library
    This will be used to overcome the single root tag limitation in Vue 2.X
    components. Once migrating to Vue 3.X the library will not be necessary.

--- dl4se-website/src/main.js
+++ dl4se-website/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueCompositionAPI from '@vue/composition-api'
+import {Plugin as VueFragment} from 'vue-fragment'
 import App from '@/App'
 import router from '@/router'
 import store from '@/store'
@@ -13,6 +14,7 @@ import '@/assets/styles/style.sass'
 Vue.config.productionTip = false
 
 Vue.use(VueCompositionAPI)
+Vue.use(VueFragment)
 Vue.use(VueAxios, axios)
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
