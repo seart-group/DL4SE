97d8d2b9cd7d4ffbd31f0c59b0cd68c383973ba2
dabico <dabico@usi.ch>
2022-06-28 23:28:22 +0200

    Added `Footer` component, not properly styled yet

--- dl4se-website/src/App.vue
+++ dl4se-website/src/App.vue
@@ -6,14 +6,18 @@
     <main id="app">
       <router-view />
     </main>
+    <footer>
+      <b-footer />
+    </footer>
   </fragment>
 </template>
 
 <script>
+import BFooter from '@/components/Footer'
 import BSmartNavbar from "@/components/SmartNavbar"
 
 export default {
-  components: { BSmartNavbar }
+  components: { BFooter, BSmartNavbar }
 }
 </script>
 
---
+++ dl4se-website/src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-container fluid class="footer-container">
+    <b-row align-h="between">
+      <b-col md="auto" sm="12">
+        <div class="footer-text">Created by:&nbsp;</div>
+        <b-link href="https://github.com/dabico" target="_blank" class="footer-link">
+          Ozren Dabić
+        </b-link>
+        <div class="footer-link-separator">,&nbsp;</div>
+        <b-link href="https://emadpres.github.io" target="_blank" class="footer-link">
+          Emad Aghajani
+        </b-link>
+        <div class="footer-link-separator">,&nbsp;</div>
+        <b-link href="https://inf.usi.ch/faculty/bavota" target="_blank" class="footer-link">
+          Gabriele Bavota
+        </b-link>
+      </b-col>
+      <b-col md="auto" sm="12">
+        <div class="footer-text">Maintained by:&nbsp;</div>
+        <b-link href="https://seart.si.usi.ch" target="_blank" class="footer-link">SEART</b-link>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "b-footer"
+}
+</script>
