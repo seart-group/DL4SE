4af361f8cac67e94b448af132adc683bd71af381
dabico <dabico@usi.ch>
2022-07-03 14:12:50 +0200

    Added custom calendar icons
    Although currently not functional, the next commits will change that,
    making the functional. We do this because we want to make them in-line
    with the rest of the icons.

---
+++ dl4se-website/src/components/IconCalendarExclamation.vue
@@ -0,0 +1,13 @@
+<!-- TODO: Make the template functional -->
+<template>
+  <b-iconstack v-bind="$attrs" v-on="$listeners">
+    <b-icon-exclamation scale="0.75" shift-v="-1" stacked />
+    <b-icon-calendar stacked />
+  </b-iconstack>
+</template>
+
+<script>
+export default {
+  name: "b-icon-calendar-exclamation"
+}
+</script>
