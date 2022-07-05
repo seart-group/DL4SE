d63a971d5e361477826cf4acd939b0e9fa686464
dabico <dabico@usi.ch>
2022-07-05 18:05:01 +0200

    Added configuration for building the app in multi-page mode
    This also includes a change to that app title

--- dl4se-website/vue.config.js
+++ dl4se-website/vue.config.js
@@ -1,6 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'DL4SE',
+      chunks: [ 'chunk-vendors', 'chunk-common', 'index' ]
+    }
+  },
   devServer: {
     port: process.env.VUE_APP_PORT
   }
