e8177e05d61b07523315de3fd8d062eb73a9bbe8
dabico <dabico@usi.ch>
2022-07-12 17:26:47 +0200

    Added confirmation modal mixin
    Similar to the toaster, we also have a mixin for dynamically portalling
    a bootstrap confirmation modal into the document. Implemented
    asynchronously, it allows us to attach `then` and `catch` blocks to
    handle the dialog result.

--- dl4se-website/src/mixins/bootstrapMixin.js
+++ dl4se-website/src/mixins/bootstrapMixin.js
@@ -11,6 +11,22 @@ export default {
                 autoHideDelay: (words(`${title} ${message}`).length / 5) * 1000 + 1000,
                 appendToast: true
             })
+        },
+        async showConfirmModal(title, message) {
+            return this.$bvModal.msgBoxConfirm(
+                message,
+                {
+                    title: title,
+                    okTitle: "Yes",
+                    okVariant: "danger",
+                    cancelTitle: "No",
+                    centered: true,
+                    noStacking: true,
+                    contentClass: "confirm-dialog-content",
+                    headerClass: "confirm-dialog-header",
+                    footerClass: "confirm-dialog-footer"
+                }
+            )
         }
     }
 }
