5ddf59616dd411b85aa737946d66e20eb12f7d18
dabico <dabico@usi.ch>
2022-07-19 11:21:08 +0200

    Added missing scroll behaviour to `router` definition

--- dl4se-website/src/router/index.js
+++ dl4se-website/src/router/index.js
@@ -101,7 +101,12 @@ const routes = [
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
+  scrollBehavior: (to) => {
+    if (to.hash) {
+      return { selector: to.hash }
+    }
+  }
 })
 
 router.beforeEach(async (to, _from, next) => {
