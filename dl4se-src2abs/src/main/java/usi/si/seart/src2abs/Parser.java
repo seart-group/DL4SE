70f7c5692c3ec6b816d0dce901aab1f33b246bd4
dabico <dabico@usi.ch>
2022-07-25 18:20:14 +0200

    `Parser` of src2abs no longer wraps methods
    It now uses the appropriate parsing method instead

--- dl4se-src2abs/src/main/java/usi/si/seart/src2abs/Parser.java
+++ dl4se-src2abs/src/main/java/usi/si/seart/src2abs/Parser.java
@@ -1,7 +1,7 @@
 package usi.si.seart.src2abs;
 
 import com.github.javaparser.StaticJavaParser;
-import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.AnnotationExpr;
 import com.github.javaparser.ast.expr.MethodCallExpr;
@@ -16,6 +16,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 @Getter
@@ -34,20 +35,21 @@ public class Parser {
 	Granularity granularity;
 
 	public void parse(String sourceCode) {
+		Function<String, Node> parser = StaticJavaParser::parse;
 		if (granularity == Granularity.METHOD) {
-			sourceCode = "public class DummyClass {" + sourceCode + "}";
+			parser = StaticJavaParser::parseMethodDeclaration;
 		}
 
 		// create compilation unit
-		CompilationUnit cu = StaticJavaParser.parse(sourceCode);
+		Node node = parser.apply(sourceCode);
 
 		// create set of annotations
-		cu.findAll(AnnotationExpr.class).stream()
+		node.findAll(AnnotationExpr.class).stream()
 				.map(AnnotationExpr::getNameAsString)
 				.forEach(annotations::add);
 
 		// create set of types
-		cu.findAll(Type.class).stream()
+		node.findAll(Type.class).stream()
 				.flatMap(type -> {
 					String[] allTypes = filterString(type.asString());
 					return Stream.of(allTypes);
@@ -55,17 +57,17 @@ public class Parser {
 				.forEach(types::add);
 
 		// create set of methods and insert declared methods
-		cu.findAll(MethodDeclaration.class).stream()
+		node.findAll(MethodDeclaration.class).stream()
 				.map(MethodDeclaration::getNameAsString)
 				.forEach(methods::add);
 
 		// insert referenced methods into methods set
-		cu.findAll(MethodCallExpr.class).stream()
+		node.findAll(MethodCallExpr.class).stream()
 				.map(MethodCallExpr::getNameAsString)
 				.forEach(methods::add);
 
 		// insert scope of methods into types
-		cu.findAll(MethodCallExpr.class).stream()
+		node.findAll(MethodCallExpr.class).stream()
 				.map(MethodCallExpr::getScope)
 				.flatMap(Optional::stream)
 				.filter(expression -> expression.isFieldAccessExpr() || expression.isNameExpr())
@@ -80,7 +82,7 @@ public class Parser {
 				});
 
 		// insert scope of methods into types
-		cu.findAll(MethodReferenceExpr.class).stream()
+		node.findAll(MethodReferenceExpr.class).stream()
 				.map(MethodReferenceExpr::getIdentifier)
 				.forEach(methods::add);
 	}
