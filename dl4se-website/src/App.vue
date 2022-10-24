c5e634985cf6b750acb9668433be62f6af050e6c
dabico <dabico@usi.ch>
2022-10-24 15:15:32 +0200

    Generified the `SmartNavbar` component
    Now it can be used cross-project. Will come especially in handy when
    we start making our own UI framework.
    
    TODO: Given that `SmartNavbar` no longer houses any data, it should be
    defined as functional to reap the rendering performance benefits.

--- dl4se-website/src/App.vue
+++ dl4se-website/src/App.vue
@@ -1,7 +1,23 @@
 <template>
   <fragment>
     <header>
-      <b-smart-navbar />
+      <b-smart-navbar brand="DL4SE"
+                      v-show="!isHomePage"
+                      :dropdown-show="isPrivatePage"
+      >
+        <template #nav-items>
+          <b-nav-item :to="{ name: 'home' }" :active="isOnPage('home')">Home</b-nav-item>
+          <b-nav-item disabled>Stats</b-nav-item>
+          <b-nav-item :to="{ name: 'about' }" :active="isOnPage('about')">About</b-nav-item>
+          <b-nav-item :to="{ name: 'docs' }" :active="isOnPage('docs')">Docs</b-nav-item>
+        </template>
+        <template #dropdown-items>
+          <b-dropdown-item disabled>Profile</b-dropdown-item>
+          <b-dropdown-item :to="{ name: 'dashboard' }">Dashboard</b-dropdown-item>
+          <b-dropdown-divider />
+          <b-dropdown-item @click="showLogOutModal">Log Out</b-dropdown-item>
+        </template>
+      </b-smart-navbar>
     </header>
     <main>
       <router-view class="router-view" />
@@ -13,11 +29,34 @@
 </template>
 
 <script>
+import bootstrapMixin from "@/mixins/bootstrapMixin"
 import BFooter from '@/components/Footer'
 import BSmartNavbar from "@/components/SmartNavbar"
 
 export default {
-  components: { BFooter, BSmartNavbar }
+  components: { BFooter, BSmartNavbar },
+  mixins: [ bootstrapMixin ],
+  computed: {
+    isHomePage() {
+      return this.isOnPage('home')
+    },
+    isPrivatePage() {
+      return !this.$route.meta.public
+    }
+  },
+  methods: {
+    isOnPage(name) {
+      return this.$route.name === name
+    },
+    showLogOutModal() {
+      this.showConfirmModal(
+          "Log Out",
+          "Any unsaved changes will be lost. Are you sure you want to continue?"
+      ).then((confirmed) => {
+        if (confirmed) this.$store.dispatch("logOut")
+      })
+    }
+  }
 }
 </script>
 
--- dl4se-website/src/components/SmartNavbar.vue
+++ dl4se-website/src/components/SmartNavbar.vue
@@ -1,25 +1,17 @@
 <template>
-  <b-navbar id="smart-navbar" v-show="!onDefaultPage"
-            class="smart-navbar" sticky toggleable="sm"
-  >
+  <b-navbar id="smart-navbar" class="smart-navbar" sticky toggleable="sm">
     <b-navbar-brand>DL4SE</b-navbar-brand>
     <b-navbar-toggle target="smart-navbar-collapse" />
     <b-collapse is-nav id="smart-navbar-collapse">
       <b-navbar-nav>
-        <b-nav-item :to="{ name: 'home' }" :active="isOnPage('home')">Home</b-nav-item>
-        <b-nav-item disabled>Stats</b-nav-item>
-        <b-nav-item :to="{ name: 'about' }" :active="isOnPage('about')">About</b-nav-item>
-        <b-nav-item :to="{ name: 'docs' }" :active="isOnPage('docs')">Docs</b-nav-item>
+        <slot name="nav-items" />
       </b-navbar-nav>
-      <b-navbar-nav v-if="!onPublicPage" class="ml-auto">
+      <b-navbar-nav v-if="dropdownShow" class="ml-auto">
         <b-nav-item-dropdown right>
           <template #button-content>
             <b-icon-person-fill />
           </template>
-          <b-dropdown-item disabled>Profile</b-dropdown-item>
-          <b-dropdown-item :to="{ name: 'dashboard' }">Dashboard</b-dropdown-item>
-          <b-dropdown-divider />
-          <b-dropdown-item @click="showLogOutModal">Log Out</b-dropdown-item>
+          <slot name="dropdown-items" />
         </b-nav-item-dropdown>
       </b-navbar-nav>
     </b-collapse>
@@ -27,30 +19,16 @@
 </template>
 
 <script>
-import bootstrapMixin from "@/mixins/bootstrapMixin"
-
 export default {
   name: "b-smart-navbar",
-  mixins: [ bootstrapMixin ],
-  computed: {
-    onDefaultPage() {
-      return this.isOnPage('home')
+  props: {
+    brand: {
+      type: String,
+      default: "App"
     },
-    onPublicPage() {
-      return this.$route.meta.public
-    }
-  },
-  methods: {
-    isOnPage(name) {
-      return this.$route.name === name
-    },
-    showLogOutModal() {
-      this.showConfirmModal(
-          "Log Out",
-          "Any unsaved changes will be lost. Are you sure you want to continue?"
-      ).then((confirmed) => {
-        if (confirmed) this.$store.dispatch("logOut")
-      })
+    dropdownShow: {
+      type: Boolean,
+      default: true
     }
   }
 }
