c07d1d2bae38f8a857fcce1c6b79aa3a677f3763
dabico <dabico@usi.ch>
2022-10-28 15:40:06 +0200

    Added queries for counting `File` and `Function` records by `Language`
    Implemented via generic JPQL to make them cross-platform

--- dl4se-server/src/main/java/usi/si/seart/repository/FunctionRepository.java
+++ dl4se-server/src/main/java/usi/si/seart/repository/FunctionRepository.java
@@ -1,7 +1,14 @@
 package usi.si.seart.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import usi.si.seart.model.code.Function;
 
-public interface FunctionRepository  extends JpaRepository<Function, Long> {
+import javax.persistence.Tuple;
+import java.util.List;
+
+public interface FunctionRepository extends JpaRepository<Function, Long> {
+
+    @Query("SELECT l, COUNT(l) FROM Function f JOIN f.language l GROUP BY l")
+    List<Tuple> countAllGroupByLanguage();
 }
