de69cce32af40050447a470b5f77f8fbf4a6740c
dabico <dabico@usi.ch>
2022-11-03 13:11:51 +0100

    Added materialized view for calculating accumulated code in bytes

--- liquibase/scripts/02_view.sql
+++ liquibase/scripts/02_view.sql
@@ -27,9 +27,13 @@ CREATE MATERIALIZED VIEW functions_by_language AS
 SELECT l.id AS lang_id, COUNT(l.id) FROM function f
 INNER JOIN language l ON f.lang_id = l.id GROUP BY l.id;
 
+CREATE MATERIALIZED VIEW code_size_in_bytes AS
+SELECT SUM(characters) AS size FROM file;
+
 CREATE UNIQUE INDEX ON table_counts("table");
 CREATE UNIQUE INDEX ON git_repos_by_language(lang_id);
 CREATE UNIQUE INDEX ON files_by_language(lang_id);
 CREATE UNIQUE INDEX ON functions_by_language(lang_id);
+CREATE UNIQUE INDEX ON code_size_in_bytes(size);
 
 INSERT INTO configuration(key, value, last_update) VALUES ('view_maintainer_cron', '0 0 0 * * *', now());
