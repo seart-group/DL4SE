a29aa4cf726ca5ffabace97b7d64cc96a950fc50
dabico <dabico@usi.ch>
2022-11-12 15:26:57 +0100

    Revert "Cards now reside in `computed` instead of `data`"
    This reverts commit da4df5b1801e1a436f3e35876d19aa7c982eaba1.

--- dl4se-website/src/views/HomeView.vue
+++ dl4se-website/src/views/HomeView.vue
@@ -30,15 +30,15 @@ export default {
   props: {
     connected: Boolean
   },
-  computed: {
-    cards() {
-      return [
-          {
-            title: "Code Completion",
-            description: "Generate a dataset for Deep Learning models specializing in code completion.",
-            linksTo: { name: "task" },
-            needsConnection: true
-          }
+  data() {
+    return {
+      cards: [
+        {
+          title: "Code Completion",
+          description: "Generate a dataset for Deep Learning models specializing in code completion.",
+          linksTo: { name: "task" },
+          needsConnection: true
+        }
       ]
     }
   }
