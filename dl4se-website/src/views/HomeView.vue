a653c3b16c16f4b0750b723f4b529d4768c0a63f
dabico <dabico@usi.ch>
2022-11-14 15:34:28 +0100

    `HomeView` iteration 2

--- dl4se-website/src/views/HomeView.vue
+++ dl4se-website/src/views/HomeView.vue
@@ -1,26 +1,62 @@
 <template>
-  <div id="home">
+  <div id="home" class="home">
     <div id="logo" class="logo fullscreen">
-      <transition name="fade" appear>
-        <div class="logo-image">
-          <span class="logo-image-negative">DL</span>
-          <span class="logo-image-positive">4SE</span>
-        </div>
-      </transition>
-      <transition name="fade" appear>
-        <div class="logo-tagline transition-delay-1000">
-          <span> Deep Learning For Software Engineering</span>
-        </div>
-      </transition>
-      <transition name="fade" appear>
-        <div class="logo-jump transition-delay-1500">
-          <b-link v-scroll-to="'#datasets'" class="logo-jump-link">
-            <b-icon-chevron-double-down />
-            Choose Your Dataset
-            <b-icon-chevron-double-down />
-          </b-link>
-        </div>
-      </transition>
+      <b-container>
+        <b-row align-h="center">
+          <b-col cols="auto">
+            <transition name="fade" appear>
+              <div class="logo-image">
+                <span class="logo-image-negative">DL</span>
+                <span class="logo-image-positive">4SE</span>
+              </div>
+            </transition>
+          </b-col>
+        </b-row>
+        <b-row align-h="center">
+          <b-col cols="auto">
+            <transition name="fade" appear>
+              <div class="logo-tagline transition-delay-1000">
+                Deep Learning For Software Engineering
+              </div>
+            </transition>
+          </b-col>
+        </b-row>
+        <b-row align-h="center">
+          <b-col cols="auto">
+            <transition name="fade" appear>
+              <div class="transition-delay-3000">
+                <b-link v-scroll-to="'#summary'" class="home-scroll-link">
+                  <b-icon-chevron-double-down />
+                  Scroll For More Info
+                  <b-icon-chevron-double-down />
+                </b-link>
+              </div>
+            </transition>
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
+    <div id="summary" class="summary fullscreen">
+      <b-container>
+        <b-row align-h="center">
+          <b-col lg="6" md="9" cols="12">
+            <p class="text-justify">
+              The DL4SE tool allows to easily create large-scale datasets that can be used to either run MSR studies or to
+              train DL models to automate SE tasks. Use our forms to define the characteristics of the dataset you would like
+              to build.
+            </p>
+          </b-col>
+        </b-row>
+        <b-row align-h="center">
+          <b-col cols="auto">
+            <b-link v-scroll-to="'#datasets'" class="home-scroll-link">
+              <b-icon-chevron-double-down />
+              Create A Dataset
+              <b-icon-chevron-double-down />
+            </b-link>
+          </b-col>
+        </b-row>
+      </b-container>
     </div>
     <div id="datasets" class="datasets fullscreen">
       <b-card v-for="({title, description, linksTo, needsConnection}, idx) in cards" :key="idx" no-body
