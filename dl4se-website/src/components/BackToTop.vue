d119c117e7bf6624c1b2b1518422739a098c2fd9
dabico <dabico@usi.ch>
2022-11-15 11:29:51 +0100

    Added a very basic `BackToTop` component
    Uses `v-scroll-to` with user-supplied target. Will only be present in
    the `HomeView` for now.
    
    TODO: Make it functional also

---
+++ dl4se-website/src/components/BackToTop.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="back-to-top">
+    <b-button v-scroll-to="target" class="back-to-top-btn">
+      <b-icon-chevron-double-up />
+    </b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BackToTop",
+  props: {
+    target: {
+      type: String,
+      required: true,
+      validator(value) {
+        return /#\S+/.test(value)
+      }
+    }
+  }
+}
+</script>
