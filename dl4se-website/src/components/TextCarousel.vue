5312fdeb481bb45a284cf273eb40fec9e088ae3b
dabico <dabico@usi.ch>
2022-11-16 18:15:03 +0100

    `TextCarousel` now paused when not visible
    Uses the `IntersectionObserver` API, so it will not work on IE but at
    this point I could not care less that browser is dead anyway.

--- dl4se-website/src/components/TextCarousel.vue
+++ dl4se-website/src/components/TextCarousel.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-carousel :interval="interval" fade no-hover-pause no-touch class="text-carousel">
+  <b-carousel :interval="currentInterval" fade no-hover-pause no-touch class="text-carousel">
     <b-carousel-slide v-for="([title, content], idx) in Object.entries(slides)" :key="idx" class="text-carousel-slide">
       <b-card no-body class="text-carousel-card">
         <b-card-body>
@@ -24,6 +24,36 @@ export default {
     interval: {
       type: Number,
       default: 10000
+    }
+  },
+  computed: {
+    currentInterval() {
+      return this.visible ? this.interval : 0
+    }
+  },
+  methods: {
+    intersectionObserverCallback(entries){
+      entries.forEach(entry => {
+        this.visible = entry.isIntersecting;
+      })
+    }
+  },
+  mounted() {
+    if (IntersectionObserver !== undefined) {
+      const observer = new IntersectionObserver(
+          this.intersectionObserverCallback,
+          {
+            root: null,
+            rootMargin: '-20px 0px',
+            threshold: 0
+          }
+      )
+      observer.observe(this.$el)
+    }
+  },
+  data() {
+    return {
+      visible: undefined
     }
   }
 }
