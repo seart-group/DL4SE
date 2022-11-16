a8042dd2a676c605f8c42a8fdf71daf4fd877b24
dabico <dabico@usi.ch>
2022-11-16 14:14:12 +0100

    Added the `DialogModal` component
    Intended to act as a generic container for navigations. Similar to the
    `DialogPage`, but in modal form.

---
+++ dl4se-website/src/components/DialogModal.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-modal :id="id" :title="title"
+           content-class="dialog-modal-content"
+           header-class="dialog-modal-header"
+           body-class="dialog-modal-body"
+           footer-class="dialog-modal-footer"
+           scrollable centered
+  >
+    <template #modal-footer>
+      <slot />
+    </template>
+  </b-modal>
+</template>
+
+<script>
+export default {
+  name: "b-dialog-modal",
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
