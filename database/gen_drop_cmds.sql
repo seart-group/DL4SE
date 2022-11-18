d6aedb2b518ec600a8392f53def8f51de07f8125
dabico <dabico@usi.ch>
2022-11-18 11:51:05 +0100

    Materialized views now also dropped as part of cleanup script

--- database/gen_drop_cmds.sql
+++ database/gen_drop_cmds.sql
@@ -3,4 +3,7 @@ FROM pg_tables
 WHERE schemaname = 'public' UNION
 SELECT 'DROP SEQUENCE IF EXISTS "' || sequencename || '" CASCADE;' AS command
 FROM pg_sequences
+WHERE schemaname = 'public' UNION
+SELECT 'DROP MATERIALIZED VIEW IF EXISTS "' || matviewname || '" CASCADE;' AS command
+FROM pg_matviews
 WHERE schemaname = 'public';
