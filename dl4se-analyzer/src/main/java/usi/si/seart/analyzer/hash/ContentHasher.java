b4b617ec5817cead1a62d8e0e740bff50840c48a
dabico <dabico@usi.ch>
2023-01-05 17:16:02 +0100

    First draft of `ContentHasher`
    The fact that I have to use a backing `bytes` field ruins the
    polymorphism. I have to find a better way to do this.

---
+++ dl4se-analyzer/src/main/java/usi/si/seart/analyzer/hash/ContentHasher.java
@@ -0,0 +1,50 @@
+package usi.si.seart.analyzer.hash;
+
+import lombok.Cleanup;
+import lombok.Getter;
+import usi.si.seart.treesitter.Node;
+import usi.si.seart.treesitter.TreeCursor;
+
+import java.util.Arrays;
+
+@Getter
+public class ContentHasher extends SHA256Hasher {
+
+    private byte[] bytes;
+    private boolean ready;
+
+    public ContentHasher() {
+        super();
+        this.bytes = new byte[0];
+        this.ready = false;
+    }
+
+    public ContentHasher(byte[] bytes) {
+        super();
+        this.bytes = bytes;
+        this.ready = true;
+    }
+
+    public void setBytes(byte[] bytes) {
+        this.bytes = bytes;
+        this.ready = bytes.length > 0;
+    }
+
+    @Override
+    protected String sha256(Node node) {
+        if (!ready) return null;
+        @Cleanup TreeCursor cursor = node.walk();
+        cursor.preorderTraversal(current -> {
+            boolean leafNode = current.getChildCount() == 0;
+            boolean isComment = current.getType().contains("comment");
+            if (leafNode && !isComment) {
+                int startByte = current.getStartByte() * 2;
+                int endByte = current.getEndByte() * 2;
+                md.update(Arrays.copyOfRange(this.bytes, startByte, endByte));
+            }
+        });
+
+        byte[] hash = md.digest();
+        return bytesToHex(hash);
+    }
+}
---
+++ dl4se-analyzer/src/test/java/usi/si/seart/analyzer/hash/ContentHasherTest.java
@@ -0,0 +1,102 @@
+package usi.si.seart.analyzer.hash;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import usi.si.seart.treesitter.Language;
+import usi.si.seart.treesitter.Parser;
+import usi.si.seart.treesitter.Tree;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+
+class ContentHasherTest extends HasherTest {
+
+    private static Parser parser;
+
+    private Tree tree;
+
+    private static final String input =
+            "package ch.usi.si;\n" +
+            "\n" +
+            "public class Main {\n" +
+            "\tpublic static void main(String[] args) {\n" +
+            "\t\t//line comment\n" +
+            "\t\tSystem.out.println(\"Hello, World!\");\n" +
+            "\t}\n" +
+            "}";
+
+    private static final String otherInput =
+            "package ch.usi.si;\n" +
+            "\n" +
+            "public class Main {\n" +
+            "\tpublic static void main(String[] args) {\n" +
+            "\t\t/*\n" +
+            "\t\t * Block comment\n" +
+            "\t\t * on multiple lines\n" +
+            "\t\t */\n" +
+            "\t\tSystem.out.println(\"Hello, World!\");\n" +
+            "\t}\n" +
+            "}";
+
+    private static final String inputFlat =
+            "packagech.usi.si;publicclassMain{publicstaticvoidmain(String[]args){System.out.println(\"Hello, World!\");}}";
+
+    private static final byte[] bytes = input.getBytes(StandardCharsets.UTF_16LE);
+    private static final byte[] otherBytes = otherInput.getBytes(StandardCharsets.UTF_16LE);
+
+    @BeforeAll
+    static void beforeAll() {
+        parser = new Parser();
+        parser.setLanguage(Language.JAVA);
+    }
+
+    @BeforeEach
+    @SneakyThrows(UnsupportedEncodingException.class)
+    void setUp() {
+        tree = parser.parseString(input);
+    }
+
+    @Test
+    void hashTest() {
+        Hasher hasher = new ContentHasher(bytes);
+        String actual = hasher.hash(tree);
+        String expected = sha256(inputFlat);
+        Assertions.assertEquals(expected, actual, "Incrementally digested tree hash should be equal to the flat code manual digest!");
+    }
+
+    @Test
+    void idempotencyTest() {
+        Hasher hasher = new ContentHasher(bytes);
+        String first = hasher.hash(tree);
+        String second = hasher.hash(tree);
+        Assertions.assertEquals(first, second, "Same instance should be reusable for multiple invocations!");
+    }
+
+    @Test
+    @SneakyThrows(UnsupportedEncodingException.class)
+    void noCommentImpactTest() {
+        Tree other = parser.parseString(otherInput);
+        ContentHasher hasher = new ContentHasher();
+        hasher.setBytes(bytes);
+        String first = hasher.hash(tree);
+        hasher.setBytes(otherBytes);
+        String second = hasher.hash(other);
+        Assertions.assertEquals(first, second, "Comments should not impact the hashing result!");
+    }
+
+    @AfterEach
+    void tearDown() {
+        tree.close();
+        tree = null;
+    }
+
+    @AfterAll
+    static void afterAll() {
+        parser.close();
+    }
+}
