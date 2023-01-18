a19dc23d169a36f41343407108ac99b8acce2f32
dabico <dabico@usi.ch>
2023-01-18 17:02:49 +0100

    Added another layer of abstraction: `PreviousCommentsAnalyzer`
    Terrible name for a class. Was never good with naming things. Don't be
    surprised if this gets changed down the line. Anyway, this new abstract
    class isolates function extraction logic that will be specific to
    languages whose comments and documentation are defined as extras that
    by convention precede the declaration. In other words the existing
    `JavaAnalyzer` as well as those that will be written for C, C++ and
    JavaScript. Python is (un?)fortunately a completely different can of
    worms.

--- dl4se-analyzer/src/main/java/usi/si/seart/analyzer/JavaAnalyzer.java
+++ dl4se-analyzer/src/main/java/usi/si/seart/analyzer/JavaAnalyzer.java
@@ -3,66 +3,16 @@ package usi.si.seart.analyzer;
 import usi.si.seart.analyzer.enumerator.JavaBoilerplateEnumerator;
 import usi.si.seart.analyzer.predicate.JavaTestFilePredicate;
 import usi.si.seart.analyzer.query.JavaSingleCaptureQueries;
-import usi.si.seart.analyzer.traverser.PreviousCommentTraverser;
-import usi.si.seart.analyzer.traverser.Traverser;
-import usi.si.seart.analyzer.util.stream.DelimiterSuffixedStringCollector;
-import usi.si.seart.model.code.Function;
 import usi.si.seart.treesitter.Language;
-import usi.si.seart.treesitter.Node;
 
 import java.nio.file.Path;
-import java.util.List;
 
-public class JavaAnalyzer extends AbstractAnalyzer {
-
-    protected Traverser<List<Node>> previousCommentTraverser;
+public class JavaAnalyzer extends PreviousCommentsAnalyzer {
 
     public JavaAnalyzer(LocalClone localClone, Path path) {
         super(localClone, path, Language.JAVA);
         this.testFilePredicate = new JavaTestFilePredicate();
         this.queries = new JavaSingleCaptureQueries();
         this.boilerplateEnumerator = new JavaBoilerplateEnumerator(this::getSourceBytes);
-        this.previousCommentTraverser = new PreviousCommentTraverser();
-    }
-
-    @Override
-    protected Function extractFunctionEntity(Node node) {
-        List<Node> comments = previousCommentTraverser.getNodes(node);
-        String doc = comments.stream()
-                .map(comment -> nodePrinter.print(comment))
-                .collect(new DelimiterSuffixedStringCollector("\n"));
-        String docAst = comments.stream()
-                .map(comment -> syntaxTreePrinter.print(comment))
-                .collect(new DelimiterSuffixedStringCollector("\n"));
-        String docSExpression = comments.stream()
-                .map(comment -> sExpressionPrinter.print(comment))
-                .collect(new DelimiterSuffixedStringCollector(" "));
-        Long docLines = comments.stream()
-                .mapToLong(comment -> lineCounter.count(comment))
-                .sum();
-        Long docTotalTokens = comments.stream()
-                .mapToLong(comment -> totalTokenCounter.count(comment))
-                .sum();
-        Long docCharacters = comments.stream()
-                .mapToLong(comment -> characterCounter.count(comment))
-                .sum();
-        boolean docContainsNonAscii = comments.stream()
-                .anyMatch(comment -> containsNonAscii.test(comment));
-
-        return Function.builder()
-                .repo(localClone.getGitRepo())
-                .content(doc + nodePrinter.print(node))
-                .contentHash(contentHasher.hash(node))
-                .ast(docAst + syntaxTreePrinter.print(node))
-                .astHash(syntaxTreeHasher.hash(node))
-                .sExpression("(sexp " + docSExpression + sExpressionPrinter.print(node)+")")
-                .totalTokens(docTotalTokens + totalTokenCounter.count(node))
-                .codeTokens(codeTokenCounter.count(node))
-                .lines(docLines + lineCounter.count(node))
-                .characters(docCharacters + characterCounter.count(node))
-                .containsNonAscii(docContainsNonAscii || containsNonAscii.test(node))
-                .containsError(containsError.test(node))
-                .boilerplateType(boilerplateEnumerator.asEnum(node))
-                .build();
     }
 }
