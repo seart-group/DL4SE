eb223aa8ec9e028882a72fd23d0d8c6979a2e75d
dabico <dabico@usi.ch>
2023-01-19 17:38:48 +0100

    Improved the `LibraryLoader` internal logic
    Will now work both in local IDE, and JAR runs

--- dl4se-tree-sitter/src/main/java/usi/si/seart/treesitter/LibraryLoader.java
+++ dl4se-tree-sitter/src/main/java/usi/si/seart/treesitter/LibraryLoader.java
@@ -1,47 +1,71 @@
 package usi.si.seart.treesitter;
 
+import lombok.AccessLevel;
+import lombok.Cleanup;
+import lombok.experimental.FieldDefaults;
 import lombok.experimental.UtilityClass;
 
-import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
 
 @UtilityClass
 public class LibraryLoader {
 
+    @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+    private static final class SystemResource {
+
+        URL url;
+        String name;
+
+        private SystemResource(String name) {
+            this.url = ClassLoader.getSystemResource(name);
+            this.name = name;
+        }
+    }
+
     public void load() {
-        String format = "libjava-tree-sitter.%s";
-        String filename;
-        switch (currentOS()) {
-            case MACOS:
-                filename = String.format(format, "dylib");
-                break;
-            case LINUX:
-                filename = String.format(format, "so");
-                break;
+        String name = "libjava-tree-sitter";
+        String extension = getExtension();
+        String filename = name + "." + extension;
+        SystemResource systemResource = new SystemResource(filename);
+        String libPath = getLibPath(systemResource);
+        System.load(libPath);
+    }
+
+    private String getLibPath(SystemResource systemResource) {
+        String protocol = systemResource.url.getProtocol();
+        switch (protocol) {
+            case "file":
+                return systemResource.url.getPath();
+            case "jar":
+                try {
+                    @Cleanup InputStream input = systemResource.url.openStream();
+                    String tmpdir = System.getProperty("java.io.tmpdir");
+                    File temporary = new File(tmpdir, systemResource.name);
+                    temporary.deleteOnExit();
+                    @Cleanup OutputStream output = new FileOutputStream(temporary, false);
+                    input.transferTo(output);
+                    return temporary.getPath();
+                } catch (IOException cause) {
+                    throw new TreeSitterException(cause);
+                }
             default:
-                throw new TreeSitterException("tree-sitter library was not compiled for this platform!");
-        }
-        URL libURL = ClassLoader.getSystemResource(filename);
-        if (libURL != null) {
-            String libPath = libURL.getPath();
-            System.load(libPath);
-        } else {
-            Exception cause = new FileNotFoundException("Resource could not be found!");
-            throw new TreeSitterException(cause);
+                Exception cause = new UnsupportedOperationException("Unsupported protocol: " + protocol);
+                throw new TreeSitterException(cause);
         }
     }
 
-    private OS currentOS() {
-        String osName = System.getProperty("os.name").toLowerCase();
-        if (osName.contains("nix") || osName.contains("nux") || osName.contains("aix"))
-            return OS.LINUX;
-        else if (osName.contains("mac") || osName.contains("darwin"))
-            return OS.MACOS;
+    private String getExtension() {
+        String platform = System.getProperty("os.name").toLowerCase();
+        if (platform.contains("nix") || platform.contains("nux") || platform.contains("aix"))
+            return "so";
+        else if (platform.contains("mac") || platform.contains("darwin"))
+            return "dylib";
         else
-            return OS.UNSUPPORTED;
-    }
-
-    private enum OS {
-        MACOS, LINUX, UNSUPPORTED
+            throw new TreeSitterException("The tree-sitter library was not compiled for this platform: " + platform);
     }
 }
