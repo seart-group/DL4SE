04089909ff8d45e2c3d246ca370174e0c090bea2
dabico <dabico@usi.ch>
2023-01-20 13:38:23 +0100

    Updated `TreeSitterException` documentation

--- dl4se-tree-sitter/src/main/java/usi/si/seart/treesitter/TreeSitterException.java
+++ dl4se-tree-sitter/src/main/java/usi/si/seart/treesitter/TreeSitterException.java
@@ -2,6 +2,9 @@ package usi.si.seart.treesitter;
 
 import lombok.experimental.StandardException;
 
+/**
+ * The base exception type for all tree-sitter exceptions.
+ */
 @StandardException
 public class TreeSitterException extends RuntimeException {
 }
