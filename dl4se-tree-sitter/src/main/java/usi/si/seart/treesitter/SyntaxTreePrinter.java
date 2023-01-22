577dd36cc74feb6913601a76a50cff3ffb41b56a
dabico <dabico@usi.ch>
2023-01-22 15:16:57 +0100

    Simplified implementation of `SyntaxTreePrinter`

--- dl4se-tree-sitter/src/main/java/usi/si/seart/treesitter/SyntaxTreePrinter.java
+++ dl4se-tree-sitter/src/main/java/usi/si/seart/treesitter/SyntaxTreePrinter.java
@@ -1,7 +1,6 @@
 package usi.si.seart.treesitter;
 
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Cleanup;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -9,14 +8,12 @@ import lombok.experimental.FieldDefaults;
 /**
  * Utility used for pretty-printing entire syntax trees, as well as their subtrees.
  */
-@AllArgsConstructor
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class SyntaxTreePrinter {
 
+    int depth = 0;
     final Node node;
-    String indentation = "  ";
-    int currentLevel = 0;
 
     /**
      * @return A string representation of the subtree. Consists only of named nodes.
@@ -26,28 +23,19 @@ public class SyntaxTreePrinter {
         StringBuilder stringBuilder = new StringBuilder();
         @Cleanup TreeCursor cursor = new TreePrinterCursor(node);
         for (;;) {
-            TreeCursorNode treeCursorNode = cursor.getCurrentTreeCursorNode();
-            if (treeCursorNode.isNamed()) {
-                String treeNode = printTreeNode(treeCursorNode);
-                stringBuilder.append(treeNode);
+            TreeCursorNode cursorNode = cursor.getCurrentTreeCursorNode();
+            if (cursorNode.isNamed()) {
+                String indentation = "  ".repeat(depth);
+                stringBuilder
+                        .append(indentation)
+                        .append(cursorNode)
+                        .append("\n");
             }
             if (cursor.gotoFirstChild() || cursor.gotoNextSibling()) continue;
             do {
                 if (!cursor.gotoParent()) return stringBuilder.toString();
             } while (!cursor.gotoNextSibling());
         }
-    }
-
-    private String printTreeNode(TreeCursorNode treeCursorNode) {
-        String name = treeCursorNode.getName();
-        String type = treeCursorNode.getType();
-        Point startPoint = treeCursorNode.getStartPoint();
-        Point endPoint = treeCursorNode.getEndPoint();
-        return String.format("%s%s%s [%s] - [%s]%n",
-                indentation.repeat(currentLevel),
-                (name != null) ? name + ": " : "",
-                type, startPoint, endPoint
-        );
     }
 
     private final class TreePrinterCursor extends TreeCursor {
@@ -59,14 +47,14 @@ public class SyntaxTreePrinter {
         @Override
         public boolean gotoFirstChild() {
             boolean success = super.gotoFirstChild();
-            if (success) currentLevel++;
+            if (success) depth++;
             return success;
         }
 
         @Override
         public boolean gotoParent() {
             boolean success = super.gotoParent();
-            if (success) currentLevel--;
+            if (success) depth--;
             return success;
         }
     }
