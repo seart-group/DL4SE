342e379d43e0a2d7c9af5e1ab1b4c8df2375c59e
dabico <dabico@usi.ch>
2023-01-27 15:01:14 +0100

    Cutting down the number of tests through expected case grouping

--- dl4se-analyzer/src/test/java/usi/si/seart/analyzer/enumerator/PythonBoilerplateEnumeratorTest.java
+++ dl4se-analyzer/src/test/java/usi/si/seart/analyzer/enumerator/PythonBoilerplateEnumeratorTest.java
@@ -17,6 +17,7 @@ import usi.si.seart.treesitter.Tree;
 
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.stream.Stream;
 
 class PythonBoilerplateEnumeratorTest extends BaseTest {
@@ -26,57 +27,92 @@ class PythonBoilerplateEnumeratorTest extends BaseTest {
         @Override
         public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) {
             return Stream.of(
-                    Arguments.of("def function():", null),
-                    Arguments.of("def __new__():", Boilerplate.CONSTRUCTOR),
-                    Arguments.of("def __init__():", Boilerplate.INITIALIZER),
-                    Arguments.of("def __str__():", Boilerplate.STRING_CONVERSION),
-                    Arguments.of("def __repr__():", Boilerplate.STRING_REPRESENTATION),
-                    Arguments.of("def __hash__():", Boilerplate.HASHER),
-                    Arguments.of("def __get__():", Boilerplate.GETTER),
-                    Arguments.of("def __getattr__():", Boilerplate.GETTER),
-                    Arguments.of("def __set__():", Boilerplate.SETTER),
-                    Arguments.of("def __setattr__():", Boilerplate.SETTER),
-                    Arguments.of("def __del__():", Boilerplate.FINALIZER),
-                    Arguments.of("def __delattr__():", Boilerplate.FINALIZER),
-                    Arguments.of("def __eq__():", Boilerplate.COMPARISON),
-                    Arguments.of("def __lt__():", Boilerplate.COMPARISON),
-                    Arguments.of("def __le__():", Boilerplate.COMPARISON),
-                    Arguments.of("def __gt__():", Boilerplate.COMPARISON),
-                    Arguments.of("def __ge__():", Boilerplate.COMPARISON),
-                    Arguments.of("def __neg__():", Boilerplate.UNARY_ARITHMETIC),
-                    Arguments.of("def __pos__():", Boilerplate.UNARY_ARITHMETIC),
-                    Arguments.of("def __abs__():", Boilerplate.UNARY_ARITHMETIC),
-                    Arguments.of("def __invert__():", Boilerplate.UNARY_ARITHMETIC),
-                    Arguments.of("def __round__():", Boilerplate.UNARY_ARITHMETIC),
-                    Arguments.of("def __floor__():", Boilerplate.UNARY_ARITHMETIC),
-                    Arguments.of("def __ceil__():", Boilerplate.UNARY_ARITHMETIC),
-                    Arguments.of("def __trunc__():", Boilerplate.UNARY_ARITHMETIC),
-                    Arguments.of("def __add__():", Boilerplate.BINARY_ARITHMETIC),
-                    Arguments.of("def __sub__():", Boilerplate.BINARY_ARITHMETIC),
-                    Arguments.of("def __mul__():", Boilerplate.BINARY_ARITHMETIC),
-                    Arguments.of("def __div__():", Boilerplate.BINARY_ARITHMETIC),
-                    Arguments.of("def __truediv__():", Boilerplate.BINARY_ARITHMETIC),
-                    Arguments.of("def __floordiv__():", Boilerplate.BINARY_ARITHMETIC),
-                    Arguments.of("def __mod__():", Boilerplate.BINARY_ARITHMETIC),
-                    Arguments.of("def __pow__():", Boilerplate.BINARY_ARITHMETIC),
-                    Arguments.of("def __lshift__():", Boilerplate.BINARY_ARITHMETIC),
-                    Arguments.of("def __rshift__():", Boilerplate.BINARY_ARITHMETIC),
-                    Arguments.of("def __and__():", Boilerplate.BINARY_ARITHMETIC),
-                    Arguments.of("def __xor__():", Boilerplate.BINARY_ARITHMETIC),
-                    Arguments.of("def __or__():", Boilerplate.BINARY_ARITHMETIC),
-                    Arguments.of("def __iadd__():", Boilerplate.AUGMENTED_ASSIGNMENT),
-                    Arguments.of("def __isub__():", Boilerplate.AUGMENTED_ASSIGNMENT),
-                    Arguments.of("def __imul__():", Boilerplate.AUGMENTED_ASSIGNMENT),
-                    Arguments.of("def __imatmul__():", Boilerplate.AUGMENTED_ASSIGNMENT),
-                    Arguments.of("def __itruediv__():", Boilerplate.AUGMENTED_ASSIGNMENT),
-                    Arguments.of("def __ifloordiv__():", Boilerplate.AUGMENTED_ASSIGNMENT),
-                    Arguments.of("def __imod__():", Boilerplate.AUGMENTED_ASSIGNMENT),
-                    Arguments.of("def __ipow__():", Boilerplate.AUGMENTED_ASSIGNMENT),
-                    Arguments.of("def __ilshift__():", Boilerplate.AUGMENTED_ASSIGNMENT),
-                    Arguments.of("def __irshift__():", Boilerplate.AUGMENTED_ASSIGNMENT),
-                    Arguments.of("def __iand__():", Boilerplate.AUGMENTED_ASSIGNMENT),
-                    Arguments.of("def __ixor__():", Boilerplate.AUGMENTED_ASSIGNMENT),
-                    Arguments.of("def __ior__():", Boilerplate.AUGMENTED_ASSIGNMENT)
+                    Arguments.of(List.of("def function():"), null),
+                    Arguments.of(List.of("def __new__():"), Boilerplate.CONSTRUCTOR),
+                    Arguments.of(List.of("def __init__():"), Boilerplate.INITIALIZER),
+                    Arguments.of(List.of("def __str__():"), Boilerplate.STRING_CONVERSION),
+                    Arguments.of(List.of("def __repr__():"), Boilerplate.STRING_REPRESENTATION),
+                    Arguments.of(List.of("def __hash__():"), Boilerplate.HASHER),
+                    Arguments.of(
+                            List.of(
+                                    "def __get__():",
+                                    "def __getattr__():"
+                            ),
+                            Boilerplate.GETTER
+                    ),
+                    Arguments.of(
+                            List.of(
+                                    "def __set__():",
+                                    "def __setattr__():"
+                            ),
+                            Boilerplate.SETTER
+                    ),
+                    Arguments.of(
+                            List.of(
+                                    "def __del__():",
+                                    "def __delattr__():"
+                            ),
+                            Boilerplate.FINALIZER
+                    ),
+                    Arguments.of(
+                            List.of(
+                                    "def __eq__():",
+                                    "def __lt__():",
+                                    "def __le__():",
+                                    "def __gt__():",
+                                    "def __ge__():"
+                            ),
+                            Boilerplate.COMPARISON
+                    ),
+                    Arguments.of(
+                            List.of(
+                                    "def __neg__():",
+                                    "def __pos__():",
+                                    "def __abs__():",
+                                    "def __invert__():",
+                                    "def __round__():",
+                                    "def __floor__():",
+                                    "def __ceil__():",
+                                    "def __trunc__():"
+                            ),
+                            Boilerplate.UNARY_ARITHMETIC
+                    ),
+                    Arguments.of(
+                            List.of(
+                                    "def __add__():",
+                                    "def __sub__():",
+                                    "def __mul__():",
+                                    "def __div__():",
+                                    "def __truediv__():",
+                                    "def __floordiv__():",
+                                    "def __mod__():",
+                                    "def __pow__():",
+                                    "def __lshift__():",
+                                    "def __rshift__():",
+                                    "def __and__():",
+                                    "def __xor__():",
+                                    "def __or__():"
+                            ),
+                            Boilerplate.BINARY_ARITHMETIC
+                    ),
+                    Arguments.of(
+                            List.of(
+                                    "def __iadd__():",
+                                    "def __isub__():",
+                                    "def __imul__():",
+                                    "def __imatmul__():",
+                                    "def __itruediv__():",
+                                    "def __ifloordiv__():",
+                                    "def __imod__():",
+                                    "def __ipow__():",
+                                    "def __ilshift__():",
+                                    "def __irshift__():",
+                                    "def __iand__():",
+                                    "def __ixor__():",
+                                    "def __ior__():"
+                            ),
+                            Boilerplate.AUGMENTED_ASSIGNMENT
+                    )
             );
         }
     }
@@ -86,16 +122,18 @@ class PythonBoilerplateEnumeratorTest extends BaseTest {
     @ParameterizedTest(name = "[{index}] {1}")
     @ArgumentsSource(PythonCodeProvider.class)
     @SneakyThrows(UnsupportedEncodingException.class)
-    void asEnumTest(String source, Boilerplate expected) {
+    void asEnumTest(List<String> sources, Boilerplate expected) {
         @Cleanup Parser parser = new Parser(Language.PYTHON);
-        @Cleanup Tree tree = parser.parseString(source);
-        Node root = tree.getRootNode();
-        Node function_definition = root.getChild(0);
-        BoilerplateEnumerator enumerator = new PythonBoilerplateEnumerator(
-                () -> source.getBytes(StandardCharsets.UTF_16LE)
-        );
-        Boilerplate actual = enumerator.asEnum(function_definition);
-        Assertions.assertEquals(expected, actual);
+        for (String source: sources) {
+            @Cleanup Tree tree = parser.parseString(source);
+            Node root = tree.getRootNode();
+            Node function_definition = root.getChild(0);
+            BoilerplateEnumerator enumerator = new PythonBoilerplateEnumerator(
+                    () -> source.getBytes(StandardCharsets.UTF_16LE)
+            );
+            Boilerplate actual = enumerator.asEnum(function_definition);
+            Assertions.assertEquals(expected, actual);
+        }
     }
 
     private static class PythonCodeProviderSpecial implements ArgumentsProvider {
