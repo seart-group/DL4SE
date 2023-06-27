3a144247d1046b6af8442792b3c4b75190d57be6
dabico <dabico@usi.ch>
2023-06-27 09:29:54 +0200

    Moved `LocalClone` to its own class

--- dl4se-analyzer/src/main/java/usi/si/seart/analyzer/Analyzer.java
+++ dl4se-analyzer/src/main/java/usi/si/seart/analyzer/Analyzer.java
@@ -4,11 +4,9 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
-import usi.si.seart.model.GitRepo;
 import usi.si.seart.model.code.File;
 import usi.si.seart.model.code.Function;
 
-import java.nio.file.Path;
 import java.util.List;
 
 public interface Analyzer extends AutoCloseable {
@@ -21,18 +19,5 @@ public interface Analyzer extends AutoCloseable {
     final class Result {
         File file;
         List<Function> functions;
-    }
-
-    @Getter
-    @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-    @AllArgsConstructor
-    final class LocalClone {
-
-        GitRepo gitRepo;
-        Path diskPath;
-
-        public Path relativePathOf(Path path) {
-            return diskPath.relativize(path);
-        }
     }
 }
