e9a6ea22eced5f5fa33d4f3f45df1ee64a0aa179
dabico <dabico@usi.ch>
2023-09-05 10:19:24 +0200

    Removed `Settings`

--- dl4se-analyzer/src/main/java/usi/si/seart/analyzer/Settings.java
+++
@@ -1,8 +0,0 @@
-package usi.si.seart.analyzer;
-
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-
-public interface Settings {
-    Charset DEFAULT_CHARSET = StandardCharsets.UTF_16LE;
-}
