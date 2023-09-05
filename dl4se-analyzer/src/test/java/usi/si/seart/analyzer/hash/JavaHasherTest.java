afcf16684da68de089e8d09069d61ed00af577c5
dabico <dabico@usi.ch>
2023-09-05 10:02:51 +0200

    Removing reference to class

--- dl4se-analyzer/src/test/java/usi/si/seart/analyzer/hash/JavaHasherTest.java
+++ dl4se-analyzer/src/test/java/usi/si/seart/analyzer/hash/JavaHasherTest.java
@@ -3,6 +3,7 @@ package usi.si.seart.analyzer.hash;
 import lombok.SneakyThrows;
 import usi.si.seart.analyzer.test.JavaBaseTest;
 
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
@@ -91,7 +92,8 @@ public abstract class JavaHasherTest extends JavaBaseTest {
     @SneakyThrows({NoSuchAlgorithmException.class})
     protected String sha256(String input) {
         MessageDigest md = MessageDigest.getInstance("SHA-256");
-        byte[] hash = md.digest(input.getBytes(SHA256Hasher.DEFAULT_CHARSET));
+        byte[] bytes = input.getBytes(StandardCharsets.UTF_16LE);
+        byte[] hash = md.digest(bytes);
         return SHA256Hasher.bytesToHex(hash);
     }
 }
