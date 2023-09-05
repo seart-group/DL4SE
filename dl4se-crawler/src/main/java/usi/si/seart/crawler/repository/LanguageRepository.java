d9d5edb2b37a0d807be0fa6521113322c8bbe1db
dabico <dabico@usi.ch>
2023-09-05 14:25:44 +0200

    `LanguageRepository` now has a `findByNameIgnoreCase` method

--- dl4se-crawler/src/main/java/usi/si/seart/crawler/repository/LanguageRepository.java
+++ dl4se-crawler/src/main/java/usi/si/seart/crawler/repository/LanguageRepository.java
@@ -3,5 +3,10 @@ package usi.si.seart.crawler.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import usi.si.seart.model.Language;
 
+import javax.validation.constraints.NotBlank;
+import java.util.Optional;
+
 public interface LanguageRepository extends JpaRepository<Language, Long> {
+
+    Optional<Language> findByNameIgnoreCase(@NotBlank String name);
 }
