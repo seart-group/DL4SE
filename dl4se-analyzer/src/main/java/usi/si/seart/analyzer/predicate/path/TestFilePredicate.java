edbd72aaa28a500f9fbd782b223dc918556327f9
dabico <dabico@usi.ch>
2023-09-05 09:43:11 +0200

    Added static factories for `predicate.path` classes

--- dl4se-analyzer/src/main/java/usi/si/seart/analyzer/predicate/path/TestFilePredicate.java
+++ dl4se-analyzer/src/main/java/usi/si/seart/analyzer/predicate/path/TestFilePredicate.java
@@ -1,5 +1,7 @@
 package usi.si.seart.analyzer.predicate.path;
 
+import ch.usi.si.seart.treesitter.Language;
+
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.PathMatcher;
@@ -12,5 +14,16 @@ public abstract class TestFilePredicate implements Predicate<Path> {
         String glob = "glob:{**/test/**,**/tests/**}";
         PathMatcher matcher = FileSystems.getDefault().getPathMatcher(glob);
         return matcher.matches(path);
+    }
+
+    public static Predicate<Path> getInstance(Language language) {
+        switch (language) {
+            case JAVA:
+                return new JavaTestFilePredicate();
+            case PYTHON:
+                return new PythonTestFilePredicate();
+            default:
+                return new TestFilePredicate() {};
+        }
     }
 }
