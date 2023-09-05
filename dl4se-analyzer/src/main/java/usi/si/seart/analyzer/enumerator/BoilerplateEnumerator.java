f495a2795e967cbe327adf0377e329b12edd6cce
dabico <dabico@usi.ch>
2023-09-05 09:46:52 +0200

    Added static factories for `enumerator` classes

--- dl4se-analyzer/src/main/java/usi/si/seart/analyzer/enumerator/BoilerplateEnumerator.java
+++ dl4se-analyzer/src/main/java/usi/si/seart/analyzer/enumerator/BoilerplateEnumerator.java
@@ -1,5 +1,6 @@
 package usi.si.seart.analyzer.enumerator;
 
+import ch.usi.si.seart.treesitter.Language;
 import usi.si.seart.analyzer.NodeMapper;
 import usi.si.seart.model.code.Boilerplate;
 
@@ -9,5 +10,16 @@ public abstract class BoilerplateEnumerator implements Enumerator<Boilerplate> {
 
     protected BoilerplateEnumerator(NodeMapper mapper) {
         this.mapper = mapper;
+    }
+
+    public static Enumerator<Boilerplate> getInstance(Language language, NodeMapper mapper) {
+        switch (language) {
+            case JAVA:
+                return new JavaBoilerplateEnumerator(mapper);
+            case PYTHON:
+                return new PythonBoilerplateEnumerator(mapper);
+            default:
+                return node -> null;
+        }
     }
 }
