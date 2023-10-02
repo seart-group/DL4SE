3b7eec4006bfb31480778df3cd40d1a8fef30e23
dabico <dabico@usi.ch>
2023-10-02 14:28:30 +0200

    Removing deprecated API usages in `MultiCaptureQueries`

--- dl4se-analyzer/src/main/java/usi/si/seart/analyzer/query/multi/MultiCaptureQueries.java
+++ dl4se-analyzer/src/main/java/usi/si/seart/analyzer/query/multi/MultiCaptureQueries.java
@@ -1,9 +1,9 @@
 package usi.si.seart.analyzer.query.multi;
 
+import ch.usi.si.seart.treesitter.Capture;
 import ch.usi.si.seart.treesitter.Language;
 import ch.usi.si.seart.treesitter.Node;
 import ch.usi.si.seart.treesitter.Query;
-import ch.usi.si.seart.treesitter.QueryCapture;
 import ch.usi.si.seart.treesitter.QueryCursor;
 import ch.usi.si.seart.treesitter.QueryMatch;
 import lombok.AccessLevel;
@@ -13,8 +13,10 @@ import org.apache.commons.lang3.tuple.Pair;
 import usi.si.seart.analyzer.query.Queries;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
@@ -27,18 +29,19 @@ public abstract class MultiCaptureQueries implements Queries<List<List<Pair<Stri
     public abstract List<List<Pair<String, Node>>> getCallableDeclarations(Node node);
 
     public List<List<Pair<String, Node>>> execute(Node node, String pattern) {
-        @Cleanup Query query = new Query(language, pattern);
+        @Cleanup Query query = Query.getFor(language, pattern);
         verify(query);
         @Cleanup QueryCursor cursor = node.walk(query);
         Stream<QueryMatch> matches = StreamSupport.stream(cursor.spliterator(), false);
         return matches.map(match -> {
-            QueryCapture[] captures = match.getCaptures();
-            List<Pair<String, Node>> tuples = new ArrayList<>(captures.length);
-            for (QueryCapture capture: captures) {
-                Node capturedNode = capture.getNode();
-                String captureName = query.getCaptureName(capture);
-                Pair<String, Node> tuple = Pair.of(captureName, capturedNode);
-                tuples.add(tuple);
+            Map<Capture, Collection<Node>> captures = match.getCaptures();
+            List<Pair<String, Node>> tuples = new ArrayList<>(captures.size());
+            for (Map.Entry<Capture, Collection<Node>> entries: captures.entrySet()) {
+                String captureName = entries.getKey().getName();
+                for (Node capturedNode: entries.getValue()) {
+                    Pair<String, Node> tuple = Pair.of(captureName, capturedNode);
+                    tuples.add(tuple);
+                }
             }
             return tuples;
         }).collect(Collectors.toList());
