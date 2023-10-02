e11e51fabbd4ac94b11e0792076f5ca15550d9c3
dabico <dabico@usi.ch>
2023-10-02 14:27:50 +0200

    Removing deprecated API usages in `SingleCaptureQueries`

--- dl4se-analyzer/src/main/java/usi/si/seart/analyzer/query/single/SingleCaptureQueries.java
+++ dl4se-analyzer/src/main/java/usi/si/seart/analyzer/query/single/SingleCaptureQueries.java
@@ -3,7 +3,6 @@ package usi.si.seart.analyzer.query.single;
 import ch.usi.si.seart.treesitter.Language;
 import ch.usi.si.seart.treesitter.Node;
 import ch.usi.si.seart.treesitter.Query;
-import ch.usi.si.seart.treesitter.QueryCapture;
 import ch.usi.si.seart.treesitter.QueryCursor;
 import ch.usi.si.seart.treesitter.QueryMatch;
 import lombok.AccessLevel;
@@ -11,7 +10,7 @@ import lombok.AllArgsConstructor;
 import lombok.Cleanup;
 import usi.si.seart.analyzer.query.Queries;
 
-import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,13 +25,12 @@ public abstract class SingleCaptureQueries implements Queries<List<Node>> {
     public abstract List<Node> getComments(Node node);
 
     public List<Node> execute(Node node, String pattern) {
-        @Cleanup Query query = new Query(language, pattern);
+        @Cleanup Query query = Query.getFor(language, pattern);
         verify(query);
         @Cleanup QueryCursor cursor = node.walk(query);
         Stream<QueryMatch> matches = StreamSupport.stream(cursor.spliterator(), false);
-        return matches.map(QueryMatch::getCaptures)
-                .flatMap(Arrays::stream)
-                .map(QueryCapture::getNode)
+        return matches.map(QueryMatch::getNodes)
+                .flatMap(Collection::stream)
                 .collect(Collectors.toList());
     }
 
