6cdbd3c3ff7764319ee2f823ff03fcaeeb6adae4
dabico <dabico@usi.ch>
2023-10-04 15:11:30 +0200

    Added new file filtering options
    We now provide optional filtering parameters for files during crawling:
    - `max-size`
    - `max-lines`
    - `max-parse-time`
    - `glob-pattern`
    
    The last option will eventually replace
    `app.crawl-job.ignore.file-pattern`

--- dl4se-crawler/src/main/java/usi/si/seart/crawler/config/CrawlerConfig.java
+++ dl4se-crawler/src/main/java/usi/si/seart/crawler/config/CrawlerConfig.java
@@ -1,19 +1,33 @@
 package usi.si.seart.crawler.config;
 
 import com.google.api.client.http.GenericUrl;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
 import usi.si.seart.model.job.Job;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.PathMatcher;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.function.Predicate;
 
 @Configuration
+@ConfigurationPropertiesScan
 public class CrawlerConfig {
 
     @Bean
@@ -36,9 +50,65 @@ public class CrawlerConfig {
         return LocalDateTime.parse(value);
     }
 
-    @Bean
-    public Predicate<Path> analyzeFilePredicate(@Value("${app.crawl-job.ignore.file-pattern}") String value) {
-        PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher("glob:" + value);
-        return Predicate.not(pathMatcher::matches);
+    @Getter
+    @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+    @ConfigurationProperties(prefix = "app.crawl-job.ignore.repository.file")
+    public static class FileFilter implements Predicate<Path> {
+
+        Predicate<Path> pathPredicate;
+
+        Optional<DataSize> maxSize;
+
+        Optional<Long> maxLines;
+
+        Optional<Duration> maxParseTime;
+
+        @ConstructorBinding
+        public FileFilter(String globPattern, DataSize maxSize, Long maxLines, Duration maxParseTime) {
+            PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:" + globPattern);
+            this.pathPredicate = Predicate.not(matcher::matches);
+            this.maxSize = Optional.ofNullable(maxSize);
+            this.maxLines = Optional.ofNullable(maxLines);
+            this.maxParseTime = Optional.ofNullable(maxParseTime);
+        }
+
+        private boolean testPath(Path path) {
+            return pathPredicate.test(path);
+        }
+
+        private boolean testSize(Path path) {
+            return maxSize.map(maxSize -> {
+                try {
+                    long bytes = Files.size(path);
+                    DataSize size = DataSize.ofBytes(bytes);
+                    return size.compareTo(maxSize) < 0;
+                } catch (IOException ex) {
+                    throw new UncheckedIOException(ex);
+                }
+            }).orElse(true);
+        }
+
+        private boolean testLine(Path path) {
+            return maxLines.map(maxLines -> {
+                try (
+                        FileReader fileReader = new FileReader(path.toFile());
+                        BufferedReader bufferedReader = new BufferedReader(fileReader)
+                ) {
+                    int lines = 0;
+                    while (bufferedReader.readLine() != null) {
+                        lines++;
+                        if (lines >= maxLines) return false;
+                    }
+                    return true;
+                } catch (IOException ex) {
+                    throw new UncheckedIOException(ex);
+                }
+            }).orElse(true);
+        }
+
+        @Override
+        public boolean test(Path path) {
+            return testPath(path) && testSize(path) && testLine(path);
+        }
     }
 }
