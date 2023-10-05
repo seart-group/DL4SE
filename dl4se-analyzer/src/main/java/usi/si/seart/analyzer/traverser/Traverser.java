f6c9bf91d5ef5d9e5cd7091f819fa2f575c97756
dabico <dabico@usi.ch>
2023-10-05 11:39:10 +0200

    Deleting the unused `Traverser` hierarchy

--- dl4se-analyzer/src/main/java/usi/si/seart/analyzer/traverser/Traverser.java
+++
@@ -1,9 +0,0 @@
-package usi.si.seart.analyzer.traverser;
-
-import ch.usi.si.seart.treesitter.Node;
-
-import java.util.Collection;
-
-public interface Traverser<C extends Collection<Node>> {
-    C getNodes(Node node);
-}
