a8295368ddb515e6d043987acd8f79caf1c0764d
dabico <dabico@usi.ch>
2023-10-09 15:04:27 +0200

    Web-related configurations are now separated into a dedicated class

--- dl4se-server/src/main/java/usi/si/seart/config/MainConfig.java
+++ dl4se-server/src/main/java/usi/si/seart/config/MainConfig.java
@@ -7,14 +7,6 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.format.FormatterRegistry;
-import org.springframework.lang.NonNull;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import usi.si.seart.converter.DtoToConfigurationConverter;
-import usi.si.seart.converter.DtoToUserConverter;
-import usi.si.seart.converter.TaskSearchDtoToSpecificationConverter;
-import usi.si.seart.converter.UserSearchDtoToSpecificationConverter;
 import usi.si.seart.jackson.PageSerializer;
 import usi.si.seart.jackson.SortSerializer;
 
@@ -46,26 +38,5 @@ public class MainConfig {
     @Bean
     public DateFormat dateFormat() {
         return new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss");
-    }
-
-    @Bean
-    public WebMvcConfigurer webConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(@NonNull final CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("*")
-                        .allowedOrigins("*")
-                        .exposedHeaders("Content-Type", "Content-Disposition", "Content-Length", "Date");
-            }
-
-            @Override
-            public void addFormatters(@NonNull final FormatterRegistry registry) {
-                registry.addConverter(new DtoToUserConverter());
-                registry.addConverter(new DtoToConfigurationConverter());
-                registry.addConverter(new TaskSearchDtoToSpecificationConverter());
-                registry.addConverter(new UserSearchDtoToSpecificationConverter());
-            }
-        };
     }
 }
---
+++ dl4se-server/src/main/java/usi/si/seart/config/WebConfig.java
@@ -0,0 +1,30 @@
+package usi.si.seart.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import usi.si.seart.converter.DtoToConfigurationConverter;
+import usi.si.seart.converter.DtoToUserConverter;
+import usi.si.seart.converter.TaskSearchDtoToSpecificationConverter;
+import usi.si.seart.converter.UserSearchDtoToSpecificationConverter;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .exposedHeaders("Content-Type", "Content-Disposition", "Content-Length", "Date");
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new DtoToUserConverter());
+        registry.addConverter(new DtoToConfigurationConverter());
+        registry.addConverter(new TaskSearchDtoToSpecificationConverter());
+        registry.addConverter(new UserSearchDtoToSpecificationConverter());
+    }
+}
