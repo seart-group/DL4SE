a738fa92f8c5e11d7aa1c32d02ccc65a7323eace
dabico <dabico@usi.ch>
2023-10-09 12:17:38 +0200

    Distinct content and ast hashes are now stored in dedicated views

---
+++ dl4se-model/src/main/java/usi/si/seart/views/code/FileAstHashDistinct.java
@@ -0,0 +1,9 @@
+package usi.si.seart.views.code;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "file_ast_hash_distinct")
+public class FileAstHashDistinct extends HashDistinct {
+}
--- liquibase/scripts/02_view.sql
+++ liquibase/scripts/02_view.sql
@@ -1,6 +1,34 @@
 -- liquibase formatted sql
 -- changeset dabico:3
 
+CREATE OR REPLACE VIEW file_content_hash_distinct AS
+SELECT
+    MIN(id) AS id,
+    content_hash AS hash
+FROM file
+GROUP BY content_hash;
+
+CREATE OR REPLACE VIEW file_ast_hash_distinct AS
+SELECT
+    MIN(id) AS id,
+    ast_hash AS hash
+FROM file
+GROUP BY ast_hash;
+
+CREATE OR REPLACE VIEW function_content_hash_distinct AS
+SELECT
+    MIN(id) AS id,
+    content_hash AS hash
+FROM function
+GROUP BY content_hash;
+
+CREATE OR REPLACE VIEW function_ast_hash_distinct AS
+SELECT
+    MIN(id) AS id,
+    ast_hash AS hash
+FROM function
+GROUP BY ast_hash;
+
 CREATE MATERIALIZED VIEW table_counts AS
 SELECT 'user' AS "table", COUNT(id) FROM "user"
 UNION
