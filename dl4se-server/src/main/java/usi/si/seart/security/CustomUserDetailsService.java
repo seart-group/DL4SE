113e9e4fd1195e0547590414b241232e3725b882
dabico <dabico@usi.ch>
2023-10-09 18:44:20 +0200

    `CustomUserDetailsService` now uses the `ConversionService`

--- dl4se-server/src/main/java/usi/si/seart/security/CustomUserDetailsService.java
+++ dl4se-server/src/main/java/usi/si/seart/security/CustomUserDetailsService.java
@@ -1,7 +1,10 @@
 package usi.si.seart.security;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,16 +13,16 @@ import usi.si.seart.repository.UserRepository;
 
 @Service
 @AllArgsConstructor(onConstructor_ = @Autowired)
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private final UserRepository userRepository;
+    UserRepository userRepository;
+    ConversionService conversionService;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByEmail(username)
-                .map(UserPrincipal::of)
-                .orElseThrow(() ->
-                        new UsernameNotFoundException("No user registered with the email: " + username)
-                );
+                .map(user -> conversionService.convert(user, UserPrincipal.class))
+                .orElseThrow(() -> new UsernameNotFoundException("No user registered with the email: " + username));
     }
 }
