f34b106c5028bfeb1a107298f88ebfb1e4081561
dabico <dabico@usi.ch>
2023-10-10 08:34:38 +0200

    Custom bean definition for `AuthenticationManager`

--- dl4se-server/src/main/java/usi/si/seart/config/SecurityConfig.java
+++ dl4se-server/src/main/java/usi/si/seart/config/SecurityConfig.java
@@ -7,13 +7,16 @@ import org.owasp.encoder.Encode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.BeanIds;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -98,9 +101,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         };
     }
 
-    @Override
-    @Bean(BeanIds.AUTHENTICATION_MANAGER)
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+    @Bean
+    public AuthenticationManager authenticationManager(
+            PasswordEncoder passwordEncoder, UserDetailsService userDetailsService
+    ) {
+        return authentication -> {
+            String username = authentication.getName();
+            String password = authentication.getCredentials().toString();
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            if (passwordEncoder.matches(password, userDetails.getPassword())) {
+                return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            } else {
+                throw new BadCredentialsException("Incorrect password!");
+            }
+        };
     }
 }
