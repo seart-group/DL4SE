1ce1efa2e803bd7869e4c4369993b36e25724638
dabico <dabico@usi.ch>
2023-10-10 11:09:25 +0200

    Re-enabling Spring Data Web support

--- dl4se-server/src/main/java/usi/si/seart/config/WebConfig.java
+++ dl4se-server/src/main/java/usi/si/seart/config/WebConfig.java
@@ -1,6 +1,7 @@
 package usi.si.seart.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +12,7 @@ import usi.si.seart.converter.UserSearchDtoToSpecificationConverter;
 import usi.si.seart.converter.UserToUserPrincipalConverter;
 
 @Configuration
+@EnableSpringDataWebSupport
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
