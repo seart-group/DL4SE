e8d5d53f06457a53740329b1e6ae8f4797dcaa1f
dabico <dabico@usi.ch>
2023-10-10 08:46:47 +0200

    Completely eliminating reliance on `WebSecurityConfigurerAdapter`
    The current version of Spring Boot we use marks it as deprecated.
    Moving away from code like this will help us when migrating to the newer
    version of the framework.

--- dl4se-server/src/main/java/usi/si/seart/config/SecurityConfig.java
+++ dl4se-server/src/main/java/usi/si/seart/config/SecurityConfig.java
@@ -9,19 +9,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import usi.si.seart.security.CustomUserDetailsService;
 import usi.si.seart.security.jwt.JwtRequestFilter;
 
 import javax.servlet.http.HttpServletResponse;
@@ -34,25 +32,20 @@ import javax.servlet.http.HttpServletResponse;
 )
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    CustomUserDetailsService userDetailsService;
-
-    JwtRequestFilter jwtRequestFilter;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors()
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity httpSecurity,
+            AuthenticationEntryPoint authenticationEntryPoint,
+            JwtRequestFilter jwtRequestFilter
+    ) throws Exception {
+        httpSecurity.cors()
                 .and()
                 .csrf()
                 .disable()
                 .exceptionHandling()
-                .authenticationEntryPoint(authenticationEntryPoint())
+                .authenticationEntryPoint(authenticationEntryPoint)
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -84,7 +77,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated();
 
-        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
     }
 
     @Bean
