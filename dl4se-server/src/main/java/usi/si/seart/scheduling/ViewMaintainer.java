f541b53480091c3ea0c90492e649e30d76a65dd6
dabico <dabico@usi.ch>
2023-10-11 11:53:40 +0200

    Improving `ViewMaintainer` scheduled job

--- dl4se-server/src/main/java/usi/si/seart/scheduling/ViewMaintainer.java
+++ dl4se-server/src/main/java/usi/si/seart/scheduling/ViewMaintainer.java
@@ -5,9 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,6 +19,11 @@ import java.util.stream.Collectors;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class ViewMaintainer implements Runnable {
 
+    private static final String SELECT = "SELECT matviewname AS material_view_name FROM pg_matviews;";
+
+    private static final String TEMPLATE = "REFRESH MATERIALIZED VIEW CONCURRENTLY %s;";
+
+    @PersistenceContext
     EntityManager entityManager;
 
     PlatformTransactionManager transactionManager;
@@ -23,14 +31,25 @@ public class ViewMaintainer implements Runnable {
     @Override
     @SuppressWarnings("unchecked")
     public void run() {
-        String selectMaterialViews = "SELECT matviewname AS material_view_name FROM pg_matviews;";
-        List<String> views = entityManager.createNativeQuery(selectMaterialViews).getResultList();
+        log.info("Recomputing statistics...");
+        List<String> views = entityManager.createNativeQuery(SELECT).getResultList();
         String statements = views.stream()
-                .map(view -> String.format("REFRESH MATERIALIZED VIEW CONCURRENTLY %s;", view))
+                .map(view -> String.format(TEMPLATE, view))
                 .collect(Collectors.joining());
-        log.debug("Refreshing materialized views...");
         TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
-        transactionTemplate.executeWithoutResult(status -> entityManager.createNativeQuery(statements).executeUpdate());
-        log.debug("Finished refreshing materialized views.");
+        transactionTemplate.execute(new TransactionCallback(statements));
+        log.info("Finished updating statistics.");
+    }
+
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+    private class TransactionCallback extends TransactionCallbackWithoutResult {
+
+        String statements;
+
+        @Override
+        protected void doInTransactionWithoutResult(TransactionStatus status) {
+            entityManager.createNativeQuery(statements).executeUpdate();
+        }
     }
 }
