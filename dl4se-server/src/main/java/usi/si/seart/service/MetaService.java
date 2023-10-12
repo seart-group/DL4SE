25d3b91ff7d73cfdd017b578120ad1b0bd04663f
dabico <dabico@usi.ch>
2023-10-12 17:01:12 +0200

    Added `MetaService` implementation dedicated to PostgreSQL

--- dl4se-server/src/main/java/usi/si/seart/service/MetaService.java
+++ dl4se-server/src/main/java/usi/si/seart/service/MetaService.java
@@ -1,7 +1,15 @@
 package usi.si.seart.service;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import usi.si.seart.meta.PostgresMaterializedView;
+import usi.si.seart.repository.PostgresMaterializedViewRepository;
 
 public interface MetaService {
 
@@ -13,6 +21,23 @@ public interface MetaService {
 
         @Override
         public void refreshMaterializedViews() {
+        }
+    }
+
+    @Service
+    @ConditionalOnProperty(value = "spring.jpa.database", havingValue = "postgresql")
+    @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+    @AllArgsConstructor(onConstructor_ = @Autowired)
+    class PostgresMetaService implements MetaService {
+
+        PostgresMaterializedViewRepository materializedViewRepository;
+
+        @Override
+        @Transactional
+        public void refreshMaterializedViews() {
+            materializedViewRepository.findAll().stream()
+                    .map(PostgresMaterializedView::getName)
+                    .forEach(materializedViewRepository::refreshByName);
         }
     }
 }
