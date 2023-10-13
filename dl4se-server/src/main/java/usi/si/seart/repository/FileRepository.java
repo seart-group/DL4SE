9bbf1511d9dec859597ae09fc82a82ad4d9a7402
dabico <dabico@usi.ch>
2023-10-13 10:24:15 +0200

    Dead code removal

--- dl4se-server/src/main/java/usi/si/seart/repository/FileRepository.java
+++ dl4se-server/src/main/java/usi/si/seart/repository/FileRepository.java
@@ -1,14 +1,7 @@
 package usi.si.seart.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import usi.si.seart.model.code.File;
 
-import javax.persistence.Tuple;
-import java.util.List;
-
 public interface FileRepository extends JpaRepository<File, Long> {
-
-    @Query("SELECT l, COUNT(l) FROM File f JOIN f.language l GROUP BY l")
-    List<Tuple> countAllGroupByLanguage();
 }
--- dl4se-server/src/main/java/usi/si/seart/repository/GitRepoRepository.java
+++ dl4se-server/src/main/java/usi/si/seart/repository/GitRepoRepository.java
@@ -1,17 +1,11 @@
 package usi.si.seart.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import usi.si.seart.model.GitRepo;
 
-import javax.persistence.Tuple;
-import java.util.List;
 import java.util.stream.Stream;
 
 public interface GitRepoRepository extends JpaRepository<GitRepo, Long> {
 
     Stream<GitRepo> streamAllBy();
-
-    @Query("SELECT l, COUNT(r) FROM GitRepo r JOIN r.languages l GROUP BY l")
-    List<Tuple> countAllGroupByLanguage();
 }
