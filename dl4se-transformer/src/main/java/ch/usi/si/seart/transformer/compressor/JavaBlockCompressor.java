683127b9f3cd3c2dba0ab4cbbe144620c91ee8dc
dabico <dabico@usi.ch>
2023-10-23 10:41:14 +0200

    Added `compressor` transformer
    Involves some degree of querying. So far, we only have one implemented
    for Java which compresses single-statement blocks by removing the curly
    braces. Does not affect empty blocks, or those with more than one
    statement, as it may lead to a change in semantics.

---
+++ dl4se-transformer/src/main/java/ch/usi/si/seart/transformer/compressor/JavaBlockCompressor.java
@@ -0,0 +1,29 @@
+package ch.usi.si.seart.transformer.compressor;
+
+import ch.usi.si.seart.treesitter.Language;
+import ch.usi.si.seart.treesitter.Node;
+
+public class JavaBlockCompressor extends NodeCompressor {
+
+    @Override
+    public Language getLanguage() {
+        return Language.JAVA;
+    }
+
+    @Override
+    protected String getQueryPattern() {
+        return "[" +
+                    "(while_statement (block . (_) .) @block)" +
+                    "(for_statement (block . (_) .) @block)" +
+                    "(if_statement (block . (_) .) @block)" +
+                    "(do_statement (block . (_) .) @block)" +
+                "]";
+    }
+
+    @Override
+    protected byte[] getTargetBytes(Node target) {
+        Node child = target.getChild(1);
+        String content = child.getContent();
+        return content.getBytes();
+    }
+}
---
+++ dl4se-transformer/src/main/java/ch/usi/si/seart/transformer/compressor/NodeCompressor.java
@@ -0,0 +1,6 @@
+package ch.usi.si.seart.transformer.compressor;
+
+import ch.usi.si.seart.transformer.QueryBasedTransformer;
+
+public abstract class NodeCompressor extends QueryBasedTransformer {
+}
