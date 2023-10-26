2a83f1e9dec0d6fde0ad0403137703416cc8242f
dabico <dabico@usi.ch>
2023-10-26 12:03:32 +0200

    `GitRepoRepository` no longer extends `ExtendedJpaRepository`
    Instead, we only extend to include the methods we need. In this case:
    - `ReadOnlyRepository`
    - `JpaStreamExecutor`

--- dl4se-server/src/main/java/ch/usi/si/seart/server/repository/GitRepoRepository.java
+++ dl4se-server/src/main/java/ch/usi/si/seart/server/repository/GitRepoRepository.java
@@ -2,13 +2,14 @@ package ch.usi.si.seart.server.repository;
 
 import ch.usi.si.seart.model.GitRepo;
 import ch.usi.si.seart.model.Language;
-import ch.usi.si.seart.server.repository.support.ExtendedJpaRepository;
+import ch.usi.si.seart.server.repository.support.JpaStreamExecutor;
+import ch.usi.si.seart.server.repository.support.ReadOnlyRepository;
 import ch.usi.si.seart.views.GroupedCount;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Collection;
 
-public interface GitRepoRepository extends ExtendedJpaRepository<GitRepo, Long> {
+public interface GitRepoRepository extends ReadOnlyRepository<GitRepo, Long>, JpaStreamExecutor<GitRepo> {
 
     @Query("SELECT _ FROM GitRepoCountByLanguage _")
     Collection<GroupedCount<Language>> countByLanguage();
