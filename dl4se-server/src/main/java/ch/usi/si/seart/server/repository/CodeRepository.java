22e3d0fc4a46fe4e608899718085e4d20dd5e193
dabico <dabico@usi.ch>
2023-10-26 12:03:58 +0200

    `CodeRepository` no longer extends `ExtendedJpaRepository`
    Instead, we only extend to include the methods we need. In this case:
    - `JpaRepository`
    - `JpaSpecificationExecutor`
    - `JpaStreamExecutor`

--- dl4se-server/src/main/java/ch/usi/si/seart/server/repository/CodeRepository.java
+++ dl4se-server/src/main/java/ch/usi/si/seart/server/repository/CodeRepository.java
@@ -1,11 +1,16 @@
 package ch.usi.si.seart.server.repository;
 
 import ch.usi.si.seart.model.code.Code;
-import ch.usi.si.seart.server.repository.support.ExtendedJpaRepository;
+import ch.usi.si.seart.server.repository.support.JpaStreamExecutor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
-public interface CodeRepository extends ExtendedJpaRepository<Code, Long> {
-
+public interface CodeRepository extends
+        JpaRepository<Code, Long>,
+        JpaSpecificationExecutor<Code>,
+        JpaStreamExecutor<Code>
+{
     @Query(value = "SELECT size FROM total_code_size_in_bytes", nativeQuery = true)
     Long size();
 }
