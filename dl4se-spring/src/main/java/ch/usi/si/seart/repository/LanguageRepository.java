3a5ef7cbb247305608942d2bbf01bf9fe46e93c0
dabico <dabico@usi.ch>
2023-10-27 21:27:46 +0200

    Removing unused `LanguageRepository` method

--- dl4se-spring/src/main/java/ch/usi/si/seart/repository/LanguageRepository.java
+++ dl4se-spring/src/main/java/ch/usi/si/seart/repository/LanguageRepository.java
@@ -8,7 +8,5 @@ import java.util.Optional;
 
 public interface LanguageRepository extends JpaRepository<Language, Long> {
 
-    Optional<Language> findByName(@NotBlank String name);
-
     Optional<Language> findByNameIgnoreCase(@NotBlank String name);
 }
