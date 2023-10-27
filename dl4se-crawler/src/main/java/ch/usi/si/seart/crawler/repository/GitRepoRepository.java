e2264d3bc117aeace6eae42cec4b0204ecf69ba4
dabico <dabico@usi.ch>
2023-10-27 11:38:55 +0200

    `GitRepoRepository` of `crawler` no longer implements unused interface

--- dl4se-crawler/src/main/java/ch/usi/si/seart/crawler/repository/GitRepoRepository.java
+++ dl4se-crawler/src/main/java/ch/usi/si/seart/crawler/repository/GitRepoRepository.java
@@ -2,14 +2,13 @@ package ch.usi.si.seart.crawler.repository;
 
 import ch.usi.si.seart.model.GitRepo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import javax.validation.constraints.NotNull;
 import java.util.Optional;
 
-public interface GitRepoRepository extends JpaRepository<GitRepo, Long>, JpaSpecificationExecutor<GitRepo> {
+public interface GitRepoRepository extends JpaRepository<GitRepo, Long> {
 
     @Query(
             "SELECT r FROM GitRepo r " +
