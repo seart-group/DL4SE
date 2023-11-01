0ddfecf9b985095de946dcc8681b54a8a3e0ca45
dabico <dabico@usi.ch>
2023-11-01 18:31:15 +0100

    Improved `ConverterConfig`

--- dl4se-crawler/src/main/java/ch/usi/si/seart/crawler/config/ConverterConfig.java
+++ dl4se-crawler/src/main/java/ch/usi/si/seart/crawler/config/ConverterConfig.java
@@ -1,33 +1,19 @@
 package ch.usi.si.seart.crawler.config;
 
-import ch.usi.si.seart.crawler.dto.SearchResultDto;
-import ch.usi.si.seart.model.GitRepo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ConversionServiceFactoryBean;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.util.unit.DataSize;
 
-import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.Set;
 
 @Configuration
 public class ConverterConfig {
 
     @Bean
-    public ConversionService conversionService(
-            Converter<Date, LocalDateTime> dateToLocalDateTimeConverter,
-            Converter<SearchResultDto, GitRepo> searchResultDtoToGitRepoConverter,
-            Converter<String, DataSize> stringToDataSizeConverter
-    ) {
+    public ConversionService conversionService(Set<Converter<?, ?>> converters) {
         ConversionServiceFactoryBean factory = new ConversionServiceFactoryBean();
-        Set<Converter<?, ?>> converters = Set.of(
-                dateToLocalDateTimeConverter,
-                searchResultDtoToGitRepoConverter,
-                stringToDataSizeConverter
-        );
         factory.setConverters(converters);
         factory.afterPropertiesSet();
         return factory.getObject();
