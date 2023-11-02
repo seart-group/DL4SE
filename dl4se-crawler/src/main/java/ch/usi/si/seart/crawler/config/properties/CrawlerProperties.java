ce77c9fd1d3e8905e019eddcb68b33492a63b242
dabico <dabico@usi.ch>
2023-11-02 22:23:08 +0100

    Added _some_ `CrawlerProperties`

---
+++ dl4se-crawler/src/main/java/ch/usi/si/seart/crawler/config/properties/CrawlerProperties.java
@@ -0,0 +1,24 @@
+package ch.usi.si.seart.crawler.config.properties;
+
+import com.google.api.client.http.GenericUrl;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+import java.time.Duration;
+
+@Getter
+@ConfigurationProperties(prefix = "app.crawl-job")
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@AllArgsConstructor(onConstructor_ = @ConstructorBinding)
+public class CrawlerProperties {
+
+    String tmpDirPrefix;
+
+    Duration nextRunDelay;
+
+    GenericUrl baseUrl;
+}
