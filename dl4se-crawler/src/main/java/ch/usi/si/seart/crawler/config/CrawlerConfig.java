f4f2a901f33c7897bec836119618a5e8038fcce4
dabico <dabico@usi.ch>
2023-11-02 22:44:13 +0100

    Added bean declaration for `FileSystemService`

--- dl4se-crawler/src/main/java/ch/usi/si/seart/crawler/config/CrawlerConfig.java
+++ dl4se-crawler/src/main/java/ch/usi/si/seart/crawler/config/CrawlerConfig.java
@@ -1,12 +1,17 @@
 package ch.usi.si.seart.crawler.config;
 
 import ch.usi.si.seart.crawler.config.properties.CrawlerProperties;
+import ch.usi.si.seart.crawler.service.FileSystemService;
 import com.google.api.client.http.GenericUrl;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
 import java.time.Duration;
 
 @Configuration
@@ -22,5 +27,17 @@ public class CrawlerConfig {
     @Scope(scopeName = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public GenericUrl baseUrl(CrawlerProperties properties) {
         return properties.getBaseUrl().clone();
+    }
+
+    @Bean
+    public FileSystemService fileSystemService(CrawlerProperties properties) {
+        String prefix = properties.getTmpDirPrefix();
+        return () -> {
+            try {
+                return Files.createTempDirectory(prefix);
+            } catch (IOException ex) {
+                throw new UncheckedIOException(ex);
+            }
+        };
     }
 }
