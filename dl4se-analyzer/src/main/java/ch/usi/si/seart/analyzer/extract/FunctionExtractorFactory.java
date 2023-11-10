3366447fef1e679dff863640912fdafdbb83e4b1
dabico <dabico@usi.ch>
2023-11-10 10:31:20 +0100

    Replace utility class with abstract class

--- dl4se-analyzer/src/main/java/ch/usi/si/seart/analyzer/extract/FunctionExtractorFactory.java
+++ dl4se-analyzer/src/main/java/ch/usi/si/seart/analyzer/extract/FunctionExtractorFactory.java
@@ -1,14 +1,12 @@
 package ch.usi.si.seart.analyzer.extract;
 
 import ch.usi.si.seart.treesitter.Language;
-import lombok.experimental.UtilityClass;
 
 import java.util.Collections;
 
-@UtilityClass
-public class FunctionExtractorFactory {
+public abstract class FunctionExtractorFactory {
 
-    public Extractor getInstance(Language language) {
+    public static Extractor getInstance(Language language) {
         switch (language) {
             case JAVA: return new JavaCallableDeclarationExtractor();
             case PYTHON: return new PythonFunctionExtractor();
