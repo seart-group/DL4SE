bcb7ee07c0b5cec912a8384750b9a1842dd8c583
dabico <dabico@usi.ch>
2024-03-07 23:38:17 +0100

    Remove redundant semicolon

--- dl4se-server/src/main/java/ch/usi/si/seart/server/exception/TokenExpiredException.java
+++ dl4se-server/src/main/java/ch/usi/si/seart/server/exception/TokenExpiredException.java
@@ -4,7 +4,7 @@ import ch.usi.si.seart.model.user.token.Token;
 
 public class TokenExpiredException extends RuntimeException {
 
-    private static final String TEMPLATE = "Token [%s] has expired on [%s]";;
+    private static final String TEMPLATE = "Token [%s] has expired on [%s]";
 
     public TokenExpiredException(Token token) {
         super(String.format(TEMPLATE, token.getValue(), token.getExpires()));
