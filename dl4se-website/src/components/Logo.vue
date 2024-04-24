1ad80713c70ff50f2ce98d5de5918a8bcb8b6c15
dabico <dabico@usi.ch>
2024-04-24 23:51:22 +0200

    Create `Logo` component for SEART

---
+++ dl4se-website/src/components/Logo.vue
@@ -0,0 +1,28 @@
+<script>
+import {BLink} from "bootstrap-vue";
+
+export default {
+  name: "b-logo",
+  functional: true,
+  components: { BLink },
+  render(createElement, { data }) {
+    return createElement(
+      BLink,
+      {
+        props: {
+          target: "_blank",
+          href: "https://seart.si.usi.ch",
+        },
+        class: {
+          logo: true,
+          ...(data.class || {}),
+        },
+      },
+      [
+        createElement("span", { class: { "logo-box": true } }, "SE"),
+        createElement("span", { class: { "logo-text": true } }, "ART"),
+      ],
+    );
+  },
+};
+</script>
