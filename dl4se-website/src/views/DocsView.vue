3705870e4b9c0b15670f3e6a0301558e26171b3d
dabico <dabico@usi.ch>
2024-04-26 23:12:37 +0200

    Update `Docs` and `About` views

--- dl4se-website/src/views/DocsView.vue
+++ dl4se-website/src/views/DocsView.vue
@@ -1,12 +1,10 @@
 <template functional>
-  <div id="documentation">
-    <h1 class="page-title">Documentation</h1>
-    <h3 id="granularity" class="page-section">Granularity</h3>
-    <div class="page-section">
-      <p class="page-text">
-        You can build your dataset by considering as a single instance either an entire file or a function:
-      </p>
-      <b-table-simple stacked="md" borderless>
+  <article class="p-5" aria-labelledby="documentation">
+    <h1 id="documentation" class="d-none">Documentation</h1>
+    <section aria-labelledby="granularity">
+      <h2 id="granularity">Granularity</h2>
+      <p>You can build your dataset by considering as a single instance either an entire file or a function:</p>
+      <b-table-simple stacked="md" borderless small>
         <b-tbody>
           <b-tr>
             <b-td><em>File</em></b-td>
@@ -20,8 +18,8 @@
           </b-tr>
         </b-tbody>
       </b-table-simple>
-      <p class="page-text">Regardless of granularity, we provide the following data for each instance:</p>
-      <b-table-simple stacked="md" borderless>
+      <p>Regardless of granularity, we provide the following data for each instance:</p>
+      <b-table-simple stacked="md" borderless small>
         <b-tbody>
           <b-tr>
             <b-td><code>content</code></b-td>
@@ -80,87 +78,68 @@
           </b-tr>
         </b-tbody>
       </b-table-simple>
-    </div>
-    <h3 id="meta" class="page-section">Metadata</h3>
-    <div class="page-section">
-      <p class="page-text">
+    </section>
+    <section aria-labelledby="meta">
+      <h2 id="meta">Metadata</h2>
+      <p>
         Certain studies/models may benefit from information such as the AST-representation of source code, which is why
         we chose to offer the option of including an Abstract Syntax Tree (<code>AST</code>) representation of each
         exported instance in Extensible Markup Language (XML) format. The inclusion of this information within the
         dataset will increase its size drastically.
       </p>
-      <p class="page-text">
+      <p>
         Alternatively, we also provide the Symbolic Expression (<code>S-Expression</code>) representation of the AST.
         This format is more compact as compared to XML, but it is also omits certain information such as the row/column
         positions of nodes in the source code. This is a more fitting format for studies/models that benefit from
         knowing the structure of the AST, without requiring exact positional information. The inclusion of this
         information within the dataset will increase its size, but not as drastically as XML.
       </p>
-      <p class="page-text">
+      <p>
         Finally, we provide the option to include the tag and semantic version of the <code>tree-sitter</code> binding
         with was used to mine the information for each instance. Given that the binding and its grammars change over
         time, this information can be useful for reproducibility purposes. Furthermore, it can be used for diagnostic
         purposes, should the user encounter any issues with the generated dataset. Given that this information is
         included on a per-instance basis, it will slightly increase the size of the dataset.
       </p>
-    </div>
-    <h3 id="filtering" class="page-section">Filtering</h3>
-    <div class="page-section">
-      <h4>Repository Filters</h4>
-      <p class="page-text">
+    </section>
+    <section aria-labelledby="repository">
+      <h2 id="repository">Repository Filters</h2>
+      <p>
         Using these parameters, you can filter the projects (repositories) from which we will extract data. The first
-        and only required criteria present in the form is the
-        <code>language</code>. Specifying a value for this filter will not match projects whose main language matches
-        the one specified, but rather all projects that have files written in that language. You can also filter
-        repositories by specifying a lower bound for specific characteristics:
+        and only required criteria present in the form is the <code>language</code>. Specifying a value for this filter
+        will not match projects whose main language matches the one specified, but rather all projects that have files
+        written in that language. You can also filter repositories by specifying a lower bound for specific
+        characteristics: <code>commits</code>, <code>contributors</code>, <code>issues</code> and <code>stars</code>. We
+        also provide options for excluding specific categories of projects from the selection, namely those that do not
+        have an <code>open source license</code> or are <code>forks</code> of other projects.
       </p>
-      <b-list-group class="mb-3">
-        <b-list-group-item
-          v-for="filter in ['commits', 'contributors', 'issues', 'stars']"
-          :key="filter"
-          class="border-0"
-        >
-          <code>{{ filter }}</code>
-        </b-list-group-item>
-      </b-list-group>
-      <p class="page-text">
-        We also provide options for excluding specific categories of projects from the selection, namely those that:
-      </p>
-      <b-list-group class="mb-3">
-        <b-list-group-item class="border-0"> Do not have an <code>open source license</code> </b-list-group-item>
-        <b-list-group-item class="border-0"> Are <code>forks</code> of other projects </b-list-group-item>
-      </b-list-group>
-    </div>
-    <div class="page-section">
-      <h4 id="exclusion">Instance Filters</h4>
-      <p class="page-text">
+    </section>
+    <section aria-labelledby="exclusion">
+      <h2 id="exclusion">Instance Filters</h2>
+      <p>
         Using these parameters, we filter the individual instances based on characteristics exhibited their the source
         code. These include closed, half-closed or unbound ranges for the minimum and maximum number of:
+        <code>characters</code>, <code>tokens</code> and <code>lines</code>.
       </p>
-      <b-list-group class="mb-3">
-        <b-list-group-item v-for="filter in ['characters', 'tokens', 'lines']" :key="filter" class="border-0">
-          <code>{{ filter }}</code>
-        </b-list-group-item>
-      </b-list-group>
-      <p class="page-text">
+      <p>
         Note that in the case of <code>tokens</code>, the column queried against depends on whether processing
         operations are applied during export. For example, removing comments from exported instances means that
         filtering will be performed according to the <code>code_tokens</code>, otherwise <code>total_tokens</code> is
-        used. Apart from these numeric attributes, you can also exclude all instances of:
+        used. Apart from these numeric attributes, you can also exclude all instances of source code that:
       </p>
-      <b-list-group class="mb-3">
-        <b-list-group-item class="border-0"> Source code considered to be test code </b-list-group-item>
-        <b-list-group-item class="border-0"> Source code containing non-ASCII characters </b-list-group-item>
-        <b-list-group-item class="border-0"> <em>Files</em> that were not successfully parsed </b-list-group-item>
-        <b-list-group-item class="border-0"> <em>Functions</em> that are considered boilerplate </b-list-group-item>
-      </b-list-group>
-    </div>
-    <div class="page-section">
-      <h4 id="duplicates-and-clones">Duplicates and Near-clones</h4>
-      <p class="page-text">
-        You can remove duplicated/similar entries from the dataset. This can be done by filtering out:
-      </p>
-      <b-table-simple stacked="md" borderless>
+      <b-table-simple stacked="md" borderless small>
+        <b-tbody>
+          <b-tr><b-td>Originates from test files</b-td></b-tr>
+          <b-tr><b-td>Contains non-ASCII characters</b-td></b-tr>
+          <b-tr><b-td>Contains syntax errors</b-td></b-tr>
+          <b-tr><b-td>Is considered boilerplate (only functions)</b-td></b-tr>
+        </b-tbody>
+      </b-table-simple>
+    </section>
+    <section aria-labelledby="duplicates-and-clones">
+      <h2 id="duplicates-and-clones">Duplicates and Near-clones</h2>
+      <p>You can remove duplicated/similar entries from the dataset. This can be done by filtering out:</p>
+      <b-table-simple stacked="md" borderless small>
         <b-tbody>
           <b-tr>
             <b-td><em>Duplicates</em></b-td>
@@ -172,75 +151,36 @@
             <b-td><em>Near-Clones</em></b-td>
             <b-td>
               Those that in spite of small differences in <code>content</code>, posses the same
-              <code>ast</code>
-              structure.
+              <code>ast</code> structure.
             </b-td>
           </b-tr>
         </b-tbody>
       </b-table-simple>
-      <p class="page-text">
+      <p>
         By definition, duplicates are a subset of near-clones, as identical contents imply identical AST structure.
         These filters have a substantial computational cost.
       </p>
-    </div>
-    <h3 id="processing" class="page-section">Processing</h3>
-    <div class="page-section">
-      <h4 id="comment-removal">Comment Removal</h4>
-      <p class="page-text">You can automatically remove two types of comments from each instance:</p>
-      <b-table-simple stacked="md" borderless>
+    </section>
+    <section aria-labelledby="processing">
+      <h2 id="processing">Processing</h2>
+      <p>
+        At the moment, we only support the removal of comments from the source code. You can automatically remove two
+        types of comments from each instance:
+      </p>
+      <b-table-simple stacked="md" borderless small>
         <b-tbody>
           <b-tr>
             <b-td><em>Inner Comments</em></b-td>
-            <b-td> Regular block or line comments found within instances. </b-td>
+            <b-td>Regular block or line comments found within instances.</b-td>
           </b-tr>
           <b-tr>
             <b-td><em>Docstrings</em></b-td>
-            <b-td> Language-specific comments used for documentation purposes (e.g., JavaDoc for Java). </b-td>
+            <b-td>
+              Language-specific comments used for documentation purposes (JavaDoc for Java, docstring for Python, etc.).
+            </b-td>
           </b-tr>
         </b-tbody>
       </b-table-simple>
-    </div>
-    <div class="page-section">
-      <h4 id="masking">Instance Masking</h4>
-      <p class="page-text">
-        This type of instance processing replaces a specified percentage of tokens with a user-specified token. It works
-        as follows:
-      </p>
-      <ol>
-        <li class="page-list-item">The source code of each processed instance is split into tokens;</li>
-        <li class="page-list-item">
-          A subset of the tokens is randomly selected for masking accordingly to the percentage specified by the user in
-          the form. Although the selection is completely random by default, checking the contiguity flag will ensure
-          that the tokens selected are contiguous;
-        </li>
-        <li class="page-list-item">
-          The selected tokens are replaced with the user-specified token (masking token), with contiguous tokens
-          replaced by only a single masking token;
-        </li>
-        <li class="page-list-item">
-          All the tokens, replaced or otherwise, are joined back together to form the new masked version of the original
-          <code>content</code>. Given that there are no guarantees as to the parsability of the masked code, we do not
-          attempt to synchronize the <code>AST</code> to match.
-        </li>
-      </ol>
-    </div>
-    <div class="page-section">
-      <h4 id="abstraction">Instance Abstraction</h4>
-      <p class="page-text">
-        Applies to each instance the abstraction process defined by Tufano et al. in their work
-        <a target="_blank" href="https://arxiv.org/abs/1812.08693">
-          An Empirical Study on Learning Bug-Fixing Patches in the Wild via Neural Machine Translation </a
-        >. The user can specify the set of idioms to consider (i.e., tokens that should not be abstracted).
-      </p>
-    </div>
-  </div>
+    </section>
+  </article>
 </template>
-
-<style scoped lang="sass">
-.page-list-item
-  margin-left: 1.5rem!important
-  margin-bottom: .5rem!important
-
-.page-text
-  text-align: justify!important
-</style>
