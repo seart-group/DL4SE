029c8e44658356753df5b79ce214dedd782942cd
dabico <dabico@usi.ch>
2024-04-26 23:39:58 +0200

    Formatting

--- dl4se-website/src/components/ConfigTable.vue
+++ dl4se-website/src/components/ConfigTable.vue
@@ -65,7 +65,7 @@
 
 <script>
 import useVuelidate from "@vuelidate/core";
-import {required} from "@vuelidate/validators";
+import { required } from "@vuelidate/validators";
 
 export default {
   name: "b-config-table",
