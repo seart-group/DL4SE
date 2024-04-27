c7c45c696b716aab1e1abfdf29bdefd5fb011ec9
dabico <dabico@usi.ch>
2024-04-27 19:17:45 +0200

    Add additional Jakarta validation annotations to repository parameters

--- dl4se-spring/src/main/java/ch/usi/si/seart/repository/TokenRepository.java
+++ dl4se-spring/src/main/java/ch/usi/si/seart/repository/TokenRepository.java
@@ -5,12 +5,13 @@ import ch.usi.si.seart.model.user.token.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.Optional;
 
 @NoRepositoryBean
 public interface TokenRepository<T extends Token> extends JpaRepository<T, Long> {
 
-    Optional<T> findByValue(String value);
-
-    Optional<T> findFirstByUser(User user);
+    Optional<T> findByValue(@NotBlank String value);
+    Optional<T> findFirstByUser(@NotNull User user);
 }
