ee3cfcc60aae03a777e9b3ee872812c18dfc30c1
dabico <dabico@usi.ch>
2024-05-01 17:49:34 +0200

    Revert "Fix link construction bug in `HateoasConfig`"
    This reverts commit a7e497b326ae11fe071639bac87eeaacb108c72d

--- dl4se-server/src/main/java/ch/usi/si/seart/server/config/HateoasConfig.java
+++ dl4se-server/src/main/java/ch/usi/si/seart/server/config/HateoasConfig.java
@@ -7,8 +7,10 @@ import ch.usi.si.seart.server.config.properties.WebsiteProperties;
 import ch.usi.si.seart.server.hateoas.URLGenerator;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.MalformedURLException;
@@ -18,31 +20,38 @@ import java.net.URL;
 public class HateoasConfig {
 
     @Bean
-    public URLGenerator<VerificationToken> verificationURLGenerator(WebsiteProperties properties) {
-        return new TokenLinkGenerator<>(properties.getBaseURL().toString(), "verify");
+    public URLGenerator<VerificationToken> verificationURLGenerator(UriComponentsBuilder uriBuilder) {
+        return new TokenLinkGenerator<>(uriBuilder, "verify");
     }
 
     @Bean
-    public URLGenerator<PasswordResetToken> passwordResetURLGenerator(WebsiteProperties properties) {
-        return new TokenLinkGenerator<>(properties.getBaseURL().toString(), "password", "reset");
+    public URLGenerator<PasswordResetToken> passwordResetURLGenerator(UriComponentsBuilder uriBuilder) {
+        return new TokenLinkGenerator<>(uriBuilder, "password", "reset");
+    }
+
+    @Bean
+    @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    UriComponentsBuilder uriBuilder(WebsiteProperties properties) {
+        return UriComponentsBuilder.fromHttpUrl(properties.getBaseURL().toString());
     }
 
     @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
     static final class TokenLinkGenerator<T extends Token> implements URLGenerator<T> {
 
-        String base;
+        UriComponentsBuilder uriComponentsBuilder;
 
         String[] prefixSegments;
 
-        public TokenLinkGenerator(String base, String... prefixSegments) {
-            this.base = base;
+        public TokenLinkGenerator(UriComponentsBuilder uriComponentsBuilder, String... prefixSegments) {
+            this.uriComponentsBuilder = uriComponentsBuilder;
             this.prefixSegments = prefixSegments;
         }
 
         @Override
         public URL generate(Token token) {
             try {
-                return UriComponentsBuilder.fromHttpUrl(base)
+                return uriComponentsBuilder
+                        .cloneBuilder()
                         .pathSegment(prefixSegments)
                         .pathSegment(token.getValue())
                         .build()
