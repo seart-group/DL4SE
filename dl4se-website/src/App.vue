2f5cf1eba8c9cbf77226a8326824f6d7ef508fa7
dabico <dabico@usi.ch>
2024-05-19 18:18:24 +0200

    Fix minor styling inconsistency with the GitHub link

--- dl4se-website/src/App.vue
+++ dl4se-website/src/App.vue
@@ -34,8 +34,8 @@
             </b-nav-item>
           </template>
           <b-nav-item href="https://github.com/seart-group/dl4se" target="_blank">
-            <b-icon-github class="d-none d-sm-inline" />
-            <span class="d-inline d-sm-none">GitHub</span>
+            <b-icon-github class="d-none d-md-inline" />
+            <span class="d-inline d-md-none">GitHub</span>
           </b-nav-item>
         </template>
       </b-smart-navbar>
