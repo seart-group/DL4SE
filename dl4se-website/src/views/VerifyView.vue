72cf291232ea52f296260b76fba46e0edfcb9e87
dabico <dabico@usi.ch>
2024-05-19 19:57:12 +0200

    Complete rewrite of `VerifyView`

--- dl4se-website/src/views/VerifyView.vue
+++ dl4se-website/src/views/VerifyView.vue
@@ -1,97 +1,97 @@
 <template>
-  <b-dialog-page id="verify" v-cloak v-if="showHtml" title="The verification link has expired." :actions="actions" />
+  <div id="verify">
+    <h1 class="d-none">Account Verification</h1>
+    <b-container>
+      <b-form v-if="failed" @submit.prevent.stop="resendToken">
+        <h2 class="text-center">Verification Link Expired</h2>
+        <b-form-row class="justify-content-center">
+          <b-form-group>
+            <b-form-submit>
+              <b-icon-arrow-clockwise shift-h="-2" rotate="45" />
+              Resend
+            </b-form-submit>
+          </b-form-group>
+        </b-form-row>
+      </b-form>
+      <b-row v-else>
+        <b-col>
+          <h2 class="text-center">Verifying Account</h2>
+          <p class="text-center">This should only take a moment.</p>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
 import routerMixin from "@/mixins/routerMixin";
-import BDialogPage from "@/components/DialogPage";
+import BFormSubmit from "@/components/FormSubmit";
 
 export default {
-  props: {
-    token: String,
-  },
-  components: { BDialogPage },
   mixins: [routerMixin],
+  components: { BFormSubmit },
+  props: {
+    token: {
+      type: String,
+      required: true,
+    },
+  },
   methods: {
     async apiCall(endpoint, successHandler, errorHandler) {
       const config = { params: { token: this.token } };
       await this.$http.get(endpoint, config).then(successHandler).catch(errorHandler);
     },
+    resendSuccess() {
+      this.redirectHomeAndToast(
+        "Token Resent",
+        "We have sent you a new verification link. Please check your email.",
+        "secondary",
+      );
+    },
+    resendError() {
+      this.redirectHomeAndToast(
+        "Server Error",
+        "An unexpected server error has occurred. Please try again later.",
+        "danger",
+      );
+    },
     async resendToken() {
-      await this.apiCall(this.resendEndpoint, this.resendSuccess, this.resendError);
+      await this.apiCall("/user/verify/resend", this.resendSuccess, this.resendError);
+    },
+    verifySuccess() {
+      this.failed = false;
+      this.redirectHomeAndToast("Account Verified", "Your account has been verified. You can now log in.", "secondary");
+    },
+    verifyError(err) {
+      const status = err.response.status;
+      switch (status) {
+        case 403:
+          break;
+        case 404:
+          this.redirectHomeAndToast(
+            "Invalid Token",
+            "The specified token does not exist. Check the link for errors and try again.",
+            "warning",
+          );
+          break;
+        default:
+          this.redirectHomeAndToast(
+            "Server Error",
+            "An unexpected server error has occurred. Please try again later.",
+            "danger",
+          );
+          break;
+      }
     },
     async verifyToken() {
-      await this.apiCall(this.verifyEndpoint, this.verifySuccess, this.verifyError);
+      await this.apiCall("/user/verify", this.verifySuccess, this.verifyError);
     },
   },
   async created() {
     await this.verifyToken();
   },
   data() {
-    return {
-      showHtml: false,
-      verifyEndpoint: "/user/verify",
-      resendEndpoint: "/user/verify/resend",
-      actions: [
-        {
-          text: "Resend",
-          icon: "arrow-clockwise",
-          action: this.resendToken,
-        },
-      ],
-      verifySuccess: () => {
-        this.redirectHomeAndToast(
-          "Account Verified",
-          "Your account has been verified. You can now log in.",
-          "secondary",
-        );
-      },
-      verifyError: (err) => {
-        const status = err.response.status;
-        let title;
-        let message;
-        let variant;
-        let action;
-        switch (status) {
-          case 403:
-            action = () => {
-              this.showHtml = true;
-            };
-            break;
-          case 404:
-            title = "Invalid Token";
-            message = "The specified token does not exist. Check the link for errors and try again.";
-            variant = "warning";
-            action = () => {
-              this.redirectHomeAndToast(title, message, variant);
-            };
-            break;
-          default:
-            title = "Server Error";
-            message = "An unexpected server error has occurred. Please try again later.";
-            variant = "danger";
-            action = () => {
-              this.redirectHomeAndToast(title, message, variant);
-            };
-            break;
-        }
-        action();
-      },
-      resendSuccess: () => {
-        this.redirectHomeAndToast(
-          "Token Resent",
-          "We have sent you a new verification link. Please check your email.",
-          "secondary",
-        );
-      },
-      resendError: () => {
-        this.redirectHomeAndToast(
-          "Server Error",
-          "An unexpected server error has occurred. Please try again later.",
-          "danger",
-        );
-      },
-    };
+    return { failed: true };
   },
 };
 </script>
