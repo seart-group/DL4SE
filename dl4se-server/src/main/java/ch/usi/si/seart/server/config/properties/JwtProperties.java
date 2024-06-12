96dc0de5ac26e4068b1d3030ea1b116f88921a09
dabico <dabico@usi.ch>
2024-06-12 13:40:22 +0200

    Server now generates a random JWT secret if non is specified
    Goes without saying that this feature is meant for development only,
    and should thus not be used in production, as each redeploy will cause
    all existing tokens to become invalidated.

--- dl4se-server/src/main/java/ch/usi/si/seart/server/config/properties/JwtProperties.java
+++ dl4se-server/src/main/java/ch/usi/si/seart/server/config/properties/JwtProperties.java
@@ -4,6 +4,8 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
@@ -14,13 +16,21 @@ import java.time.Duration;
 @Getter
 @ConfigurationProperties(prefix = "platform.jwt", ignoreUnknownFields = false)
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-@AllArgsConstructor(onConstructor_ = @ConstructorBinding)
 public class JwtProperties {
+
+    private static final int MIN_LENGTH = 32;
+    private static final String DEFAULT = RandomStringUtils.random(MIN_LENGTH, true, true);
 
     @NotBlank
     String secret;
 
     TokenProperties tokens;
+
+    @ConstructorBinding
+    public JwtProperties(String secret, TokenProperties tokens) {
+        this.secret = StringUtils.isBlank(secret) ? DEFAULT : secret;
+        this.tokens = tokens;
+    }
 
     public TokenProperties getTokenProperties() {
         return tokens;
