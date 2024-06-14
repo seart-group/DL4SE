ceee92b5e491db123680c4cb2fb88b1376ae9f63
dabico <dabico@usi.ch>
2024-06-14 15:39:09 +0200

    Introduce `Abbr` component
    Builds upon the styling of `abbr` from BootStrap while also making use
    of its tooltips for more responsive feedback.

---
+++ dl4se-website/src/components/Abbr.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  name: "b-abbr",
+  functional: true,
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  render(createElement, { props, data, children }) {
+    return createElement(
+      "abbr",
+      {
+        attrs: data.attrs,
+        class: data.staticClass,
+        on: data.on,
+        directives: [
+          {
+            name: "b-tooltip",
+            rawName: "v-b-tooltip.html",
+            value: props.title,
+            modifiers: {
+              html: true,
+            },
+          },
+          ...(data.directives ?? []),
+        ],
+      },
+      children,
+    );
+  },
+};
+</script>
+
+<style scoped lang="sass" src="@/assets/styles/component/abbr.sass" />
