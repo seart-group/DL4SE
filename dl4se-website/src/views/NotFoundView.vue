cacf608121438d45533a8490e02f1470dac7e637
dabico <dabico@usi.ch>
2024-06-15 21:09:01 +0200

    Replace `functional` views with regular views containing partials
    The purpose of this pattern is to retain the same rendering performance
    advantages while also allowing these pages to have their `script` tag
    settings. This will further pave the way to using other functional
    components within these views.

--- dl4se-website/src/views/NotFoundView.vue
+++ dl4se-website/src/views/NotFoundView.vue
@@ -1,22 +1,11 @@
+<template>
+  <not-found />
+</template>
+
 <script>
+import NotFound from "@/partials/NotFound";
+
 export default {
-  functional: true,
-  render() {
-    return (
-      <div id="not-found">
-        <h1 class="d-none">Not Found</h1>
-        <b-container>
-          <b-row>
-            <b-col>
-              <h2 class="text-center">Page Not Found</h2>
-              <p class="text-center">
-                The requested webpage can not be found, as it does not exist, or has been moved.
-              </p>
-            </b-col>
-          </b-row>
-        </b-container>
-      </div>
-    );
-  },
+  components: { NotFound },
 };
 </script>
