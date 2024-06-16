2f5c4dcaf26792ab069e7f2d96974967b1c1c657
dabico <dabico@usi.ch>
2024-06-16 11:59:08 +0200

    Tune `manifest.json` file

--- dl4se-website/vue.config.js
+++ dl4se-website/vue.config.js
@@ -11,6 +11,37 @@ module.exports = defineConfig({
       chunks: ["chunk-vendors", "chunk-common", "index"],
     },
   },
+  pwa: {
+    name: "SEART Data Hub",
+    themeColor: "#F8F9FA",
+    manifestOptions: {
+      short_name: "Data Hub",
+      description:
+        "Building Training Datasets for Deep Learning Models in Software Engineering and Empirical Software Engineering Research",
+      categories: [
+        "DL4SE",
+        "deep learning",
+        "software engineering",
+        "deep learning for software engineering",
+        "empirical software engineering",
+        "mining software repositories",
+        "mining source code",
+        "dataset generation",
+        "training datasets",
+        "research",
+      ],
+      screenshots: [],
+      start_url: "/",
+      background_color: "#FFFFFF",
+      display: "minimal-ui",
+      orientation: "any",
+      dir: "ltr",
+      lang: "en",
+      launch_handler: {
+        client_mode: "auto",
+      },
+    },
+  },
   devServer: {
     port: process.env.VUE_APP_PORT,
   },
