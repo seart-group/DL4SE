dd2718ecf6c982ed5f6fb3e705ba1d7eb361ee4e
dabico <dabico@usi.ch>
2024-06-16 23:24:03 +0200

    Remove log-only hooks from service worker registration

--- dl4se-website/src/sw/index.js
+++ dl4se-website/src/sw/index.js
@@ -1,27 +1,8 @@
-/* eslint-disable no-console */
-
 import { register } from "register-service-worker";
 
 if (process.env.NODE_ENV === "production") {
   register(`${process.env.BASE_URL}service-worker.js`, {
-    ready() {
-      console.log(
-        "App is being served from cache by a service worker.\n" + "For more details, visit https://goo.gl/AFskqB",
-      );
-    },
-    registered() {
-      console.log("Service worker has been registered.");
-    },
-    cached() {
-      console.log("Content has been cached for offline use.");
-    },
-    updatefound() {
-      console.log("New content is downloading.");
-    },
     updated: (registration) => document.dispatchEvent(new CustomEvent("swUpdated", { detail: registration })),
-    offline() {
-      console.log("No internet connection found. App is running in offline mode.");
-    },
     error: ({ message }) => document.dispatchEvent(new CustomEvent("swError", { detail: message })),
   });
 }
