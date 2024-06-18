588ab596000c3c5ec9df3c9a8880ea1cdc0b91b1
dabico <dabico@usi.ch>
2024-06-18 11:28:17 +0200

    Further simplification of the `v-aos` directive

--- dl4se-website/src/directives/v-aos.js
+++ dl4se-website/src/directives/v-aos.js
@@ -1,14 +1,14 @@
 export default (el, binding) => {
   const config = binding.value || {};
   const modifiers = binding.modifiers || {};
-  Object.entries({
-    "data-aos": config.animation,
-    "data-aos-offset": `${config.offset ?? 120}`,
-    "data-aos-delay": `${config.delay ?? 0}`,
-    "data-aos-duration": `${config.duration ?? 400}`,
-    "data-aos-easing": config.easing || "ease",
-    "data-aos-anchor-placement": config.anchorPlacement || "top-bottom",
-    "data-aos-once": `${!!modifiers.once}`,
-    "data-aos-mirror": `${!!modifiers.mirror}`,
-  }).forEach(([key, value]) => el.setAttribute(key, value));
+  Object.assign(el.dataset, {
+    aos: config.animation,
+    aosOffset: `${config.offset ?? 120}`,
+    aosDelay: `${config.delay ?? 0}`,
+    aosDuration: `${config.duration ?? 400}`,
+    aosEasing: config.easing || "ease",
+    aosAnchorPlacement: config.anchorPlacement || "top-bottom",
+    aosOnce: `${!!modifiers.once}`,
+    aosMirror: `${!!modifiers.mirror}`,
+  });
 };
