b06f5c9311f8d771794c6daa96441ac559a25fe5
dabico <dabico@usi.ch>
2024-06-21 15:39:16 +0200

    Add `PreformattedCode` component

---
+++ dl4se-website/src/components/PreformattedCode.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "b-preformatted-code",
+  functional: true,
+  render(createElement, { data, children }) {
+    return createElement(
+      "pre",
+      {
+        class: data.staticClass,
+        style: data.staticStyle,
+      },
+      [createElement("code", { class: "text-monospace" }, children)],
+    );
+  },
+};
+</script>
+
+<style scoped lang="sass" src="@/assets/styles/component/preformatted-code.sass" />
