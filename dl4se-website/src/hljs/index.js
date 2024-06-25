5402bd70aebfb7bf3bcccb608947a7a7dae3cf8f
dabico <dabico@usi.ch>
2024-06-25 16:38:01 +0200

    Register default languages for HLJS

---
+++ dl4se-website/src/hljs/index.js
@@ -0,0 +1,10 @@
+import hljs from "highlight.js/lib/core";
+import java from "highlight.js/lib/languages/java";
+import python from "highlight.js/lib/languages/python";
+import json from "highlight.js/lib/languages/json";
+import xml from "highlight.js/lib/languages/xml";
+
+hljs.registerLanguage("java", java);
+hljs.registerLanguage("python", python);
+hljs.registerLanguage("json", json);
+hljs.registerLanguage("xml", xml);
--- dl4se-website/src/main.js
+++ dl4se-website/src/main.js
@@ -9,10 +9,12 @@ import unhead from "@/unhead";
 import VueAxios from "vue-axios";
 import VueScreen from "vue-screen";
 import VueScrollTo from "vue-scrollto";
+import VueHighlight from "@highlightjs/vue-plugin";
 import { UnheadPlugin as VueUnhead } from "@unhead/vue/vue2";
 import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
 import "@/assets/styles/style.sass";
 import "@/sw";
+import "@/hljs";
 
 Vue.config.productionTip = false;
 
