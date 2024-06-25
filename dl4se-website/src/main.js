7b1b4e7135209fd333668ceaeae682488dc31a6f
dabico <dabico@usi.ch>
2024-06-25 16:41:19 +0200

    Downgrade HLJS and add official Vue 2 plugin
    > [!NOTE]
    > We can not use a newer version for the plugin. If there are two
    > different dependencies registered, the plugin will cease to work.

--- dl4se-website/src/main.js
+++ dl4se-website/src/main.js
@@ -24,6 +24,7 @@ Vue.use(VueUnhead);
 Vue.use(VueAxios, axios);
 Vue.use(VueScreen, "bootstrap");
 Vue.use(VueScrollTo);
+Vue.use(VueHighlight);
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
 
